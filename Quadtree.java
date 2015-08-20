8767caca64527f1d9203779dac9c41b72c76c603
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-08-20 16:24:41 -0400

    Update Quadtree.java

--- Quadtree.java
+++ Quadtree.java
@@ -1,1 +1,9 @@
 
+/*
+ * A quadtree is a tree data structure where every internal node has four children
+ * It is used to partition a two dimensional space into quadrants
+ */
+
+public class Quadtree {
+  
+}
