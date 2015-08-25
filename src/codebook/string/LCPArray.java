8a9959fa31744d9dc18b62077234ddccf0808b34
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-08-25 13:01:32 -0400

    Create LCPArray.java

---
+++ src/codebook/string/LCPArray.java
@@ -0,0 +1,47 @@
+/*
+ * O(N) time complexity to generate the LCP array of a string. Dependent on the suffix array.
+ */
+package codebook.string;
+
+public class LCPArray {
+  private String text;
+  private int len;
+  
+  private int[] LCP;
+  
+  LCPArray (String text) {
+    this.text = text;
+    this.len = text.length();
+    this.LCP = new int[len];
+    computeLCP();
+  }
+  public String getString () {
+    return text;
+  }
+  public void setString (String s) {
+    this.text = s;
+    this.len = text.length();
+    this.LCP = new int[len];
+    computeLCP();
+  }
+  private computeLCP () {
+    int[] sa = new SuffixArraySort(text).getSuffixArray();
+    int[] rank = new int[len];
+    for (int i = 0; i < len; i++)
+      rank[sa[i]] = i;
+    int k = 0;
+    for (int i = 0; i < len; i++, k = k > 0 ? k - 1 : 0) {
+      if (rank[i] == n-1) {
+        k = 0;
+        continue;
+      }
+      int j = sa[rank[i]+1]
+      while (j + k < len && i + k < len && text.charAt(j+k) == text.charAt(i+k))
+        k++;
+      LCP[rank[i]] = k;
+    }
+  }
+  public int[] getLCP () {
+    return LCP;
+  }
+}
