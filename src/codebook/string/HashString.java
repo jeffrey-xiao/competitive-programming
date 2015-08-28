528e11963eca335546a7b8e469e58865a9fdab42
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-08-27 21:44:33 -0400

    Update

---
+++ src/codebook/string/HashString.java
@@ -0,0 +1,46 @@
+// 1-indexed hash string
+
+package codebook.string;
+
+public class HashString {
+	
+	static final int BASE = 137;
+	static final int MOD = 1000000007;
+	
+	private String text;
+	private long[] hash;
+	private long[] pow;
+	
+	HashString (String text) {
+		this.text = " " + text;
+		initialize();
+	}
+	
+	public void setText (String text) {
+		this.text = text;
+		initialize();
+	}
+	
+	public String getText () {
+		return text;
+	}
+	
+	private void initialize () {
+		hash = new long[text.length()];
+		pow[0] = 1;
+		hash[0] = 0;
+		for (int i = 1; i < text.length(); i++) {
+			pow[i] = (pow[i-1] * BASE) % MOD;
+			hash[i] = (hash[i-1] * BASE + text.charAt(i)) % MOD;
+		}
+	}
+	
+	public long getHash () {
+		return getHash(1, text.length()-1);
+	}
+	
+	public long getHash (int l, int r) {
+		return (hash[r] - hash[l - 1] * pow[r - (l - 1)] % MOD + MOD) % MOD;
+	}
+}
+
