ca0a628876c4bdda9d3fccb580ad3b7dc38cc713
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-08-28 11:31:32 -0400

    Commented

--- src/codebook/string/ZAlgorithm.java
+++ src/codebook/string/ZAlgorithm.java
@@ -24,8 +24,10 @@ public class ZAlgorithm {
 
 	private void compute () {
 		z = new int[text.length()];
+		// [l, r] represents the largest prefix substring of the previous index
 		int l = 0, r = 0;
 		for (int i = 1; i < text.length(); i++) {
+			// if the current index exceeds the right boundary of the previous prefix substring, then reset it
 			if (i > r) {
 				l = r = i;
 				while (r < text.length() && text.charAt(r) == text.charAt(r - l))
@@ -33,10 +35,15 @@ public class ZAlgorithm {
 				r--;
 				z[i] = r - l + 1;
 			} else {
+				// S[j] will be the same character as S[i]
+				// if the size of the prefix substring at j is smaller than the boundary,
+				// then z[i] = z[j]
 				int j = i - l;
 				if (z[j] < r - i + 1)
 					z[i] = z[j];
+				// if the size of the prefix substring at j is larger than the boundary, then we have to extend it
 				else {
+				
 					l = i;
 					while (r < text.length() && text.charAt(r) == text.charAt(r - l))
 						r++;
