9171a72caca960e3a279739baab1bb463b8c8649
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-08-28 10:26:00 -0400

    Commented

--- src/codebook/string/KMP.java
+++ src/codebook/string/KMP.java
@@ -22,10 +22,12 @@ public class KMP {
 	public int search (String text) {
 		int j = 0;
 		for (int i = 0; i < text.length(); i++) {
+			// loop until we find a prefix whose next character matches the current character are on
 			while (j > 0 && text.charAt(i) != pattern.charAt(j))
 				j = LCP[j - 1];
 			if (text.charAt(i) == pattern.charAt(j))
 				j++;
+			// the entire pattern has been matched
 			if (j == pattern.length())
 				return i - j + 1;
 		}
@@ -35,11 +37,12 @@ public class KMP {
 	private void buildLCP () {
 		LCP = new int[pattern.length()];
 		for (int i = 1; i < pattern.length(); i++) {
+			// attempt to build on the previous LCP
 			int j = LCP[i - 1];
-
+			// loop until we find a prefix whose next character matches the current character we are on
 			while (j > 0 && pattern.charAt(i) != pattern.charAt(j))
 				j = LCP[j - 1];
-
+			
 			if (pattern.charAt(i) == pattern.charAt(j))
 				j++;
 
