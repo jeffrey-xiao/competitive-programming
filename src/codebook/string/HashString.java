90b20c04acb42a16baf7705555cb792704e52ee1
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-08-28 10:19:15 -0400

    Commented

--- src/codebook/string/HashString.java
+++ src/codebook/string/HashString.java
@@ -38,7 +38,10 @@ public class HashString {
 	public long getHash () {
 		return getHash(1, text.length()-1);
 	}
-	
+	// hash[0, l] = S[0] * BASE ^ l + S[1] * BASE ^ (l-1) + ... + S[l-1] * BASE ^ 1 + S[l] * BASE ^ 0
+	// hash[0, r] = S[0] * BASE ^ r + S[1] * BASE ^ (r-1) + ... + S[r-1] * BASE ^ 1 + S[r] * BASE ^ 0
+	// Since we want the hash of [l, r], we must get rid of the hash of [0, l-1]
+	// The hash of [0, l-1] in hash[0, r] is BASE^(r - (l - 1)) times greater than the hash of [0, l-1] in hash[0, r]
 	public long getHash (int l, int r) {
 		return (hash[r] - hash[l - 1] * pow[r - (l - 1)] % MOD + MOD) % MOD;
 	}
