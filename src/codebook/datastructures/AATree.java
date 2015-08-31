0ac7a31a505e582e673801e521bab86e91518493
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-08-31 14:32:22 -0400

    Update AATree.java

--- src/codebook/datastructures/AATree.java
+++ src/codebook/datastructures/AATree.java
@@ -1,4 +1,5 @@
-/* An AA Tree is a balanced binary search tree that is a variation of the red-black tree with an additional restriction.
+/* 
+ * An AA Tree is a balanced binary search tree that is a variation of the red-black tree with an additional restriction.
  * Unlike red-black trees, no red node can be left subchild. This results in the simulation of 2-3 tree instead of a 2-3-4 tree.
  */
 package codebook.datastructures;
