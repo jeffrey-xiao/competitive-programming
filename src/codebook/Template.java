67071f0832fbdd958f0ce45afd90e8b778d54d64
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-08-31 09:39:20 -0400

    Update Template.java

--- src/codebook/Template.java
+++ src/codebook/Template.java
@@ -15,11 +15,11 @@ public class Template {
 
 	public static void main (String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
-		pr = new PrintWriter(new OutputStreamWriter(System.out));
-		//br = new BufferedReader(new FileReader("in.txt"));
-		//pr = new PrintWriter(new FileWriter("out.txt"));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		// br = new BufferedReader(new FileReader("in.txt"));
+		// out = new PrintWriter(new FileWriter("out.txt"));
 
-		pr.close();
+		out.close();
 	}
 
 	static String next () throws IOException {
