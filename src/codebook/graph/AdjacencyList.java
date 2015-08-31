137d42126079ec1a4419e5128621f8d9424f84f1
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-08-31 14:59:46 -0400

    Create AdjacencyList.java

---
+++ src/codebook/graph/AdjacencyList.java
@@ -0,0 +1,84 @@
+/*
+ * An adjacency list is a data structure that represents a graph.
+ */
+
+package codebook.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+
+public class AdjacencyList {
+
+	static BufferedReader br;
+	static PrintWriter out;
+	static StringTokenizer st;
+
+	static ArrayList<ArrayList<Edge>> adj = new ArrayList<ArrayList<Edge>>();
+	static int n, m;
+	
+	public static void main (String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		//br = new BufferedReader(new FileReader("in.txt"));
+		//out = new PrintWriter(new FileWriter("out.txt"));
+		
+		n = readInt();
+		m = readInt();
+		
+		for (int i = 0; i < n; i++) 
+			adj.add(new ArrayList<Edge>());
+		
+		for (int i = 0; i < m; i++) {
+			int a = readInt() - 1;
+			int b = readInt() - 1;
+			int c = readInt();
+			adj.get(a).add(new Edge(b, c));
+			adj.get(b).add(new Edge(a, c));
+		}
+		
+		for (int i = 0; i < n; i++) {
+			out.print(i + " IS CONNECTED TO: ");
+			for (Edge j : adj.get(i))
+				out.print(j.dest + " ");
+			out.println();
+		}
+		out.close();
+	}
+	static class Edge {
+		int dest, cost;
+		Edge (int dest, int cost) {
+			this.dest = dest;
+			this.cost = cost;
+		}
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
