8ef5c2aefb48e3aff3da3eaf05d0b51c19f8ab8c
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-09-01 13:44:24 -0400

    Update MisofTree.cpp

--- src/codebook/datastructures/MisofTree.cpp
+++ src/codebook/datastructures/MisofTree.cpp
@@ -1,21 +1,21 @@
 int tree[17][65536];
 void insert(int x) { 
-  for (int i=0; i<17; i++) { 
-    tree[i][x]++; x/=2;
-  } 
+	for (int i=0; i<17; i++) { 
+		tree[i][x]++; x/=2;
+	} 
 }
 void erase(int x) { 
-  for (int i=0; i<17; i++) { 
-    tree[i][x]--; x/=2; 
-  } 
+	for (int i=0; i<17; i++) { 
+		tree[i][x]--; x/=2; 
+	} 
 }
 int kThElement(int k) { 
-  int a=0, 
-  b=16;
-  while (b--) { 
-    a*=2; 
-    if (tree[b][a]<k) 
-      k-=tree[b][a++]; 
-  }
-  return a; 
+	int a=0, 
+	b=16;
+	while (b--) { 
+		a*=2; 
+		if (tree[b][a]<k) 
+			k-=tree[b][a++]; 
+	}
+	return a; 
 }
