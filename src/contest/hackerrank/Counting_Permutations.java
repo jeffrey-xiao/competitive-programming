7039127b996631a6e36a8f38c6b06799a75bd4c1
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-09-05 00:26:08 -0400

    Initial Solution

---
+++ src/contest/hackerrank/Counting_Permutations.java
@@ -0,0 +1,179 @@
+package contest.hackerrank;
+
+import java.util.*;
+import java.io.*;
+
+public class Counting_Permutations {
+
+	static BufferedReader br;
+	static PrintWriter out;
+	static StringTokenizer st;
+	static final int MOD = 1000000007;
+	static Node[] tree;
+	static boolean[] dup;
+	static long[] F;
+	public static void main (String[] args) throws IOException {
+//		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		br = new BufferedReader(new FileReader("in.txt"));
+		//out = new PrintWriter(new FileWriter("out.txt"));
+		F = new long[100001];
+		F[0] = 1;
+		for (int i = 1; i <= 100000; i++) {
+			F[i] = F[i-1]*i % MOD;
+		}
+		
+		int t = readInt();
+		for (int qq = 0; qq < t; qq++) {
+			int n = readInt();
+			tree = new Node[n];
+			dup = new boolean[n];
+			for (int i = 0; i < n; i++)
+				tree[i] = new Node();
+			for (int i = 0; i < n - 1; i++) {
+				int a = readInt()-1;
+				int b = readInt()-1;
+				tree[a].child.add(b);
+				tree[b].child.add(a);
+			}
+			for (int i = 0; i < n; i++)
+				if (tree[i].child.size() <= 2)
+					compute(i, -1);
+			for (int i = 0; i < n; i++)
+				if (tree[i].child.size() <= 2)
+					dp(i, -1);
+			HashSet<String> v = new HashSet<String>();
+			long ans = 0;
+			for (int i = 0; i < n; i++) {
+//				out.println(i + " " + tree[i].hash.get(-1));
+				if (tree[i].child.size() <= 2 && !v.contains(tree[i].hash.get(-1).toString())) {
+//					out.println("DP ON " + i + " " + tree[i].dp.get(-1));
+					ans = (ans + tree[i].dp.get(-1))%MOD;
+					v.add(tree[i].hash.get(-1).toString());
+				}
+			}
+			out.println(ans);
+		}
+		
+		
+		out.close();
+	}
+	private static long choose (int n, int k) {
+		return divMod(divMod(F[n], F[k]), F[n-k]);
+	}
+	private static long divMod (long i, long j) {
+		return i * pow(j, MOD-2)%MOD;
+	}
+	private static long pow (long a, int b) {
+		if (b == 0)
+			return 1;
+		if (b == 1)
+			return a;
+		if (b % 2 == 0)
+			return pow(a*a % MOD, b/2);
+		return a*pow(a*a % MOD, b/2)%MOD;
+	}
+	private static void dp (int i, int prev) {
+		if (tree[i].dp.containsKey(prev))
+			return;
+		ArrayList<Integer> dpv = new ArrayList<Integer>();
+		for (int j : tree[i].child) {
+			if (j != prev) {
+				dp(j, i);
+				dpv.add(j);
+			}
+		}
+		if (dpv.size() == 0)
+			tree[i].dp.put(prev, 1l);
+		else if (dpv.size() == 1)
+			tree[i].dp.put(prev, 2*tree[dpv.get(0)].dp.get(i) % MOD);
+		else if (dpv.size() == 2) {
+			int a = tree[dpv.get(0)].size.get(i);
+			int b = tree[dpv.get(1)].size.get(i);
+			long dpa = tree[dpv.get(0)].dp.get(i);
+			long dpb = tree[dpv.get(1)].dp.get(i);
+			long pow = tree[dpv.get(0)].hash.get(i).toString().equals(tree[dpv.get(1)].hash.get(i).toString()) ? 1 : 2;
+			tree[i].dp.put(prev, (((choose(a+b, a) * dpa)%MOD * dpb)%MOD * pow) % MOD);
+		}
+			
+	}
+	private static void compute (int i, int prev) {
+		if (tree[i].hash.containsKey(prev))
+			return;
+//		System.out.println(i + " " + prev);
+		int size = 1;
+		ArrayList<StringBuilder> hv = new ArrayList<StringBuilder>();
+		for (int j : tree[i].child)
+			if (j != prev) {
+				compute(j, i);
+				size += tree[j].size.get(i);
+				hv.add(tree[j].hash.get(i));
+			}
+		tree[i].size.put(prev, size);
+		if (hv.size() == 0)
+			tree[i].hash.put(prev, new StringBuilder());
+		else if (hv.size() == 2) {
+			if (hv.get(0).toString().compareTo(hv.get(1).toString()) < 0) {
+				tree[i].hash.put(prev, new StringBuilder());
+				tree[i].hash.get(prev).append("0");
+				tree[i].hash.get(prev).append(hv.get(0));
+				tree[i].hash.get(prev).append("21");
+				tree[i].hash.get(prev).append(hv.get(1));
+				tree[i].hash.get(prev).append("2");
+			}
+			else {
+				tree[i].hash.put(prev, new StringBuilder());
+				tree[i].hash.get(prev).append("0");
+				tree[i].hash.get(prev).append(hv.get(1));
+				tree[i].hash.get(prev).append("21");
+				tree[i].hash.get(prev).append(hv.get(0));
+				tree[i].hash.get(prev).append("2");
+			}
+		} else {
+			tree[i].hash.put(prev, new StringBuilder());
+			tree[i].hash.get(prev).append("0");
+			tree[i].hash.get(prev).append(hv.get(0));
+			tree[i].hash.get(prev).append("2");
+		}
+	}
+
+	static class Node {
+		ArrayList<Integer> child;
+		HashMap<Integer, Long> dp;
+		HashMap<Integer, StringBuilder> hash;
+		HashMap<Integer, Integer> size;
+		Node () {
+			child = new ArrayList<Integer>();
+			dp = new HashMap<Integer, Long>();
+			hash = new HashMap<Integer, StringBuilder>();
+			size = new HashMap<Integer, Integer>();
+		}
+	}
+	
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
+
---
+++ src/contest/hackerrank/Debug.java
@@ -0,0 +1,331 @@
+package contest.hackerrank;
+
+import java.util.*;
+import java.io.*;
+
+public class Debug {
+
+	static BufferedReader br;
+	static PrintWriter out;
+	static StringTokenizer st;
+
+	static int n, m;
+	static int[][] g;
+	static final long MOD = 1000000007;
+	static ArrayList<HashMap<State, Long>> dp;
+	static ArrayList<ArrayList<Integer>> indexes;
+	static boolean[][] v;
+	static int[] dsu;
+	public static void main (String[] args) throws IOException {
+//		long ans1 = 0;
+//		long ans2 = 0;
+//		while (ans1 == ans2) {
+//		PrintWriter o = new PrintWriter(new FileWriter("in.txt"));
+//		o.println(5);
+//		o.println(5);
+//		for (int i = 0; i < 5; i++) {
+//			String res = "";
+//			for (int j = 0; j < 5; j++) {
+//				int rand = (int)(Math.random()*3);
+//				if (rand == 0)
+//					res += '.';
+//				else if (rand == 1)
+//					res += '#';
+//				else
+//					res += '?';
+//			}
+//			o.println(res);
+//		}
+//		o.close();
+//		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		br = new BufferedReader(new FileReader("in.txt"));
+		//out = new PrintWriter(new FileWriter("out.txt"));
+
+
+		n = readInt();
+		m = readInt();
+		g = new int[n][m];
+		indexes = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i < n; i++) {
+			String in = next();
+			indexes.add(new ArrayList<Integer>());
+			for (int j = 0; j < m; j++) {
+				if (in.charAt(j) == '.')
+					g[i][j] = -1;
+				else if (in.charAt(j) == '#')
+					g[i][j] = 0;
+				else {
+					g[i][j] = -2;
+					indexes.get(i).add(j);
+				}
+			}
+		}
+//		out.println(bf());
+		dp = new ArrayList<HashMap<State, Long>>();
+		dp.add(new HashMap<State, Long>());
+		dp.add(new HashMap<State, Long>());
+		for (int i = 0; i < n; i++) {
+			int sz = indexes.get(i).size();
+			dp.get(i%2).clear();
+//			out.println(i);
+			for (int j = 0; j < (1 << sz); j++) {
+				int[] newRow = new int[m];
+				for (int k = 0; k < m; k++)
+					newRow[k] = g[i][k];
+				for (int k = 0; k < sz; k++) {
+					newRow[indexes.get(i).get(k)] = (j & 1 << k) > 0 ? -1 : 0;
+				}
+				int id = 1;
+				for (int k = 0; k < m; k++) {
+					if (newRow[k] == -1)
+						newRow[k] = id++;
+					if (k+1 < m && newRow[k] != 0 && newRow[k+1] == -1)
+						newRow[k+1] = newRow[k];
+				}
+				
+				if (i == 0) {
+//					out.println("NEW ROW");
+//					for (int k = 0; k < m; k++)
+//						out.print(newRow[k] + " ");
+//					out.println();
+					dp.get(i).put(new State(newRow, true), 1l);
+				} else {
+					int[] ogrow = Arrays.copyOf(newRow, newRow.length);
+					main : for (Map.Entry<State, Long> e : dp.get((i - 1)%2).entrySet()) {
+						newRow = Arrays.copyOf(ogrow, ogrow.length);
+						State s = e.getKey();
+						dsu = new int[m];
+						HashSet<Integer> hs = new HashSet<Integer>();
+						for (int k = 0; k < m; k++) {
+							hs.add(s.row[k]);
+							dsu[k] = k;
+						}
+						for (int k = 0; k < m - 1; k++) {
+							if (newRow[k] == newRow[k + 1] && newRow[k] != 0)
+								merge(k, k+1);
+						}
+						for (int k = 0; k < m; k++) {
+							for (int l = k + 1; l < m; l++) {
+								if (s.row[k] != 0 && s.row[l] != 0 && newRow[k] != 0 && newRow[l] != 0 && s.row[k] == s.row[l] && newRow[k] == newRow[l]) {
+									continue main;
+								}
+							}
+						}
+						
+						boolean hasCurr = false;
+						boolean hasPrev = false;
+						for (int k = 0; k < m; k++) {
+							for (int l = k + 1; l < m; l++) {
+								if (newRow[k] != 0 && newRow[l] != 0 && s.row[k] != 0 && s.row[l] != 0 && s.row[k] == s.row[l]) {
+									merge(k, l);
+								}
+							}
+							if (!s.canEmpty && newRow[k] != 0)
+								continue main;
+							if (newRow[k] != 0)
+								hasCurr = true;
+							if (s.row[k] != 0)
+								hasPrev = true;
+						}
+						for (Integer k : hs) {
+							if (k == 0)
+								continue;
+							boolean valid = false;
+							for (int l = 0; l < m; l++) {
+								if (s.row[l] == k && newRow[l] != 0)
+									valid = true;
+							}
+							if (!valid && hasCurr)
+								continue main;
+						}
+						if (!hasCurr && hs.size() > 2)
+							continue main;
+						
+						for (int k = 0; k < m; k++)
+							if (newRow[k] != 0)
+								newRow[k] = find(k) + 1;
+						id = 1;
+						HashMap<Integer, Integer> convert = new HashMap<Integer, Integer>();
+						for (int k = 0; k < m; k++) {
+							if (newRow[k] != 0 && !convert.containsKey(newRow[k]))
+								convert.put(newRow[k], id++);
+							if (newRow[k] != 0)
+								newRow[k] = convert.get(newRow[k]);
+						}
+//						out.println("OLD ROWWWW");
+//						for (int k = 0; k < m; k++)
+//							out.print(s.row[k] + " ");
+//						out.println();
+//						out.println("NEW ROW");
+//						for (int k = 0; k < m; k++)
+//							out.print(newRow[k] + " ");
+//						out.println();
+						boolean canEmpty = false;
+						if (hasCurr)
+							canEmpty = true;
+						else if (!hasCurr && hasPrev)
+							canEmpty = false;
+						else if (!hasCurr && !hasPrev)
+							canEmpty = s.canEmpty;
+//						out.println(canEmpty);
+						add(i % 2, new State(newRow, canEmpty), e.getValue());
+					}
+				}
+			}
+		}
+		long ans = 0;
+		for (Map.Entry<State, Long> e : dp.get((n - 1) % 2).entrySet()) {
+			HashSet<Integer> hs = new HashSet<Integer>();
+			State s = e.getKey();
+			for (int i = 0; i < m; i++)
+				hs.add(s.row[i]);
+			if (hs.size() <= 2) {
+				for (int i = 0; i < m; i++) {
+					out.print(s.row[i] + " ");
+				}
+				out.println("\n " + e.getValue());
+				ans = (ans + e.getValue())%MOD;
+			}
+		}
+		out.println(ans);
+		out.close();
+//		ans1 = ans;
+//		ans2 = bf();
+//		}
+	}
+	private static int find (int x) {
+		return x == dsu[x] ? x : (dsu[x] = find(dsu[x]));
+	}
+	private static void merge (int x, int y) {
+		int rx = find(x);
+		int ry = find(y);
+		dsu[rx] = ry;
+	}
+	private static int bf () {
+
+		ArrayList<Point> p = new ArrayList<Point>();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (g[i][j] == -2)
+					p.add(new Point(i, j));
+			}
+		}
+		int res = 0;
+		for (int i = 0; i < (1 << p.size()); i++) {
+			for (int j = 0; j < p.size(); j++) {
+				if ((i & 1 << j) > 0) {
+					g[p.get(j).x][p.get(j).y] = -1;
+				} else {
+					g[p.get(j).x][p.get(j).y] = 0;
+				}
+			}
+			int cnt = 0;
+			v = new boolean[n][m];
+			boolean valid = true;
+			for (int k = 0; k < n; k++) {
+				for (int l = 0; l < m; l++) {
+					if (!v[k][l] && g[k][l] != 0) {
+						valid &= dfs(k, l, -1, -1);
+						cnt++;
+					}
+				}
+			}
+			if (cnt <= 1 && valid) {
+//				for (int k = 0; k < n; k++) {
+//					for (int l = 0; l < m; l++) {
+//						out.print(g[k][l] != 0 ? 1 : 0);
+//					}
+//					out.println();
+//				}
+//				out.println();
+				res++;
+			}
+		}
+		return res;
+
+	}
+	static int[] movex = {0,0,-1,1};
+	static int[] movey = {-1,1,0,0};
+	static boolean dfs (int i, int j, int pi, int pj) {
+		v[i][j] = true;
+		boolean valid = true;
+		for (int z = 0; z < 4; z++) {
+			int ni = i + movex[z];
+			int nj = j + movey[z];
+//			out.println("NEXT " + ni + " " + nj);
+			if (ni < 0 || nj < 0 || ni >= n || nj >= m || (ni == pi && nj == pj) || g[ni][nj] == 0)
+				continue;
+			if (v[ni][nj]) {
+				valid = false;
+				continue;
+			}
+			valid &= dfs(ni, nj, i, j);
+		}	
+		return valid;
+	}
+	static class Point {
+		int x, y;
+		Point (int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	static void add (int i, State s, long cnt) {
+		if (!dp.get(i).containsKey(s))
+			dp.get(i).put(s, 0l);
+		dp.get(i).put(s, (dp.get(i).get(s) + cnt)%MOD);
+	}
+	static class State {
+		int[] row;
+		boolean canEmpty;
+		State (int[] row, boolean canEmpty) {
+			this.row = Arrays.copyOf(row, row.length);
+			this.canEmpty = canEmpty;
+		}
+		public int hashCode () {
+			int res = 0;
+			for (int i = 0; i < row.length; i++)
+				res = (res * 31 + row[i]);
+			return res * 31 + (canEmpty ? 1 : 0);
+		}
+		public boolean equals (Object o) {
+			if (o instanceof State) {
+				State s = (State)o;
+				for (int i = 0; i < row.length; i++)
+					if (s.row[i] != row[i])
+						return false;
+				return canEmpty == s.canEmpty;
+			}
+			return false;
+		}
+
+	}
+
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
+
