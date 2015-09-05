d1a18364a6e99a1c4e317e3c7b3111fee227cb4f
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-09-05 11:13:46 -0400

    Comments

--- src/codebook/string/PalindromeTreeSimple.java
+++ src/codebook/string/PalindromeTreeSimple.java
@@ -11,10 +11,13 @@ public class PalindromeTreeSimple {
 	private Node root, zero;
 	private int ans = 0;
 
+	// initializing the tree and the roots
 	PalindromeTreeSimple () {
 		tree = new ArrayList<Node>();
 		seq = new ArrayList<Character>();
+		// the root has a length of negative one for convenience
 		root = new Node(0, -1);
+		// the "zero root" will compute the palindromes of even length
 		zero = new Node(0, 0);
 		root.suffixLink = root;
 		zero.suffixLink = root;
@@ -41,12 +44,18 @@ public class PalindromeTreeSimple {
 	public void addCharacter (char c) {
 		seq.add(c);
 		Node curr = tree.get(tree.size() - 1);
+		// find the suffix to extend so that when we add character c it will be a palindrome
 		while (seq.size() - curr.len - 2 < 0 || seq.get(seq.size() - curr.len - 2) != c)
 			curr = curr.suffixLink;
+		// after we found the suffix, we check if the palindrome node has already been created
 		Node next = curr.nextLink[c - SHIFT];
 		if (next == null) {
+			// if the new palindrome node has not been created, then create one
 			next = (curr.nextLink[c - SHIFT] = new Node());
+			// initialize the length
 			next.len = curr.len + 2;
+			// if the length is one, then we set the suffix to the zero root to compute even length palindromes in the future
+			// otherwise, we find find suffix link of the new palindrome node
 			if (next.len == 1) {
 				next.suffixLink = zero;
 				next.num = 1;
@@ -59,12 +68,16 @@ public class PalindromeTreeSimple {
 				next.num = curr.num + 1;
 			}
 		}
+		// add the number of palindromes contained in our new suffix palindrome
 		ans += next.num;
+		// add the suffix into our tree for removal later
 		tree.add(next);
 	}
 
 	public void deleteCharacter () {
+		// subtract the answer from the last palindrome node
 		ans -= tree.get(tree.size() - 1).num;
+		// remove the palindrome node and the last character added
 		tree.remove(tree.size() - 1);
 		seq.remove(tree.size() - 1);
 	}
--- src/codebook/string/PalindromeTreeSmart.java
+++ src/codebook/string/PalindromeTreeSmart.java
@@ -11,10 +11,13 @@ public class PalindromeTreeSmart {
 	private Node root, zero;
 	private int ans = 0;
 
+	// initializing the tree and the roots
 	PalindromeTreeSmart () {
 		tree = new ArrayList<Node>();
 		seq = new ArrayList<Character>();
+		// the root has a length of negative one for convenience
 		root = new Node(0, -1);
+		// the "zero root" will compute the palindromes of even length
 		zero = new Node(0, 0);
 		root.suffixLink = root.smartLink = root;
 		zero.suffixLink = zero.smartLink = root;
@@ -22,6 +25,8 @@ public class PalindromeTreeSmart {
 	}
 
 	class Node {
+		// suffix link goes to the next suffix
+		// smart link goes to the next suffix with a different character
 		private int num, len;
 		private Node suffixLink;
 		private Node smartLink;
@@ -41,7 +46,10 @@ public class PalindromeTreeSmart {
 	}
 
 	private Node getMatchingSuffix (Node n) {
+		// while we haven't found the new suffix, we travel down the smart and suffix links
 		while (seq.size() - n.len - 2 < 0 || seq.get(seq.size() - n.len - 2) != seq.get(seq.size() - 1)) {
+			// if the next suffix link leads to an appropriate suffix, then we go down the suffix link
+			// else we go down the smart link
 			if (seq.size() - n.suffixLink.len - 2 >= 0 && seq.get(seq.size() - n.suffixLink.len - 2) == seq.get(seq.size() - 1))
 				n = n.suffixLink;
 			else
@@ -52,29 +60,39 @@ public class PalindromeTreeSmart {
 
 	public void addCharacter (char c) {
 		seq.add(c);
+		// find the suffix to extend so that when we add character c it will be a palindrome
 		Node curr = getMatchingSuffix(tree.get(tree.size() - 1));
 		Node next = curr.nextLink[c - SHIFT];
+		// after we found our suffix to extend on, we check if the new palindrome has already been created
 		if (next == null) {
+			// if it hasn't then we just create a new palindrome node
 			next = (curr.nextLink[c - SHIFT] = new Node());
 			next.len = curr.len + 2;
+			// if the length is one, we create a suffix link to the zero root to process any even length palindromes in the future
+			// else we find the next palindrome suffix and create a suffix link to it
 			if (next.len == 1)
 				next.suffixLink = zero;
 			else
 				next.suffixLink = getMatchingSuffix(curr.suffixLink).nextLink[c - SHIFT];
 			int len1 = seq.size() - next.suffixLink.len - 1;
 			int len2 = seq.size() - next.suffixLink.suffixLink.len - 1;
+			// if the suffix begins with the same character, then the smart link will be the smart link of the suffix
+			// else the smart link will just point the suffix
 			if (len1 >= 0 && len2 >= 0 && len1 < seq.size() && len2 < seq.size() && seq.get(len1) == seq.get(len2))
 				next.smartLink = next.suffixLink.smartLink;
 			else
 				next.smartLink = next.suffixLink;
 			next.num = next.suffixLink.num + 1;
 		}
+		// add the current palindrome node to the tree and increment the answer by the number of palindromes contained by the node
 		tree.add(next);
 		ans += next.num;
 	}
 
 	public void deleteCharacter () {
+		// subtract answer by the number of palindromes contained by the node to delete
 		ans -= tree.get(tree.size() - 1).num;
+		// delete the node and the previous character added
 		tree.remove(tree.size() - 1);
 		seq.remove(tree.size() - 1);
 	}
