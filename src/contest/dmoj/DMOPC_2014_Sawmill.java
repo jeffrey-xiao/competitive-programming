594afb2176ac388292d1b463706ac5164abcdd1e
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-09-16 23:38:44 -0400

    Added DMOPC Sawmill

---
+++ src/contest/dmoj/DMOPC_2014_Sawmill.java
@@ -0,0 +1,60 @@
+package contest.dmoj;
+
+import java.util.*;
+import java.io.*;
+
+public class DMOPC_2014_Sawmill {
+
+	static BufferedReader br;
+	static PrintWriter out;
+	static StringTokenizer st;
+
+	public static void main (String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		//br = new BufferedReader(new FileReader("in.txt"));
+		//out = new PrintWriter(new FileWriter("out.txt"));
+
+		int n = readInt();
+		int[] a = new int[n];
+		int[] b = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = readInt();
+		for (int i = 0; i < n; i++)
+			b[i] = readInt();
+		int ans = 0;
+		Arrays.sort(a);
+		Arrays.sort(b);
+		for (int i = 0; i < n; i++)
+			ans += a[i] * b[n - i - 1];
+		out.println(ans);
+		out.close();
+	}
+
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
+
