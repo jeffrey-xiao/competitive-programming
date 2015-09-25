ff7a9f3ba43cc144d3c8237d34e70331a9087248
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-09-25 10:20:09 -0400

    Initial Commit

--- src/codebook/graph/network/MinCostMaxMatching.java
+++ src/codebook/graph/network/MinCostMaxMatching.java
@@ -1,61 +1,30 @@
-/*
- * An adjacency list is a data structure that represents a graph.
- */
+package codebook.graph.network;
 
-package codebook.graph.representation;
+import java.util.*;
+import java.io.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
-public class AdjacencyList {
+public class MinCostMaxMatching {
 
 	static BufferedReader br;
 	static PrintWriter out;
 	static StringTokenizer st;
 
-	static ArrayList<ArrayList<Edge>> adj = new ArrayList<ArrayList<Edge>>();
-	static int n, m;
-
+	static int n;
+	static int[][] m;
+	
 	public static void main (String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		out = new PrintWriter(new OutputStreamWriter(System.out));
 		//br = new BufferedReader(new FileReader("in.txt"));
 		//out = new PrintWriter(new FileWriter("out.txt"));
 
+		// precondition: a square matrix where the rows represent workers and the columns represents jobs
 		n = readInt();
-		m = readInt();
-
+		m = new int[n][n];
 		for (int i = 0; i < n; i++)
-			adj.add(new ArrayList<Edge>());
-
-		for (int i = 0; i < m; i++) {
-			int a = readInt() - 1;
-			int b = readInt() - 1;
-			int c = readInt();
-			adj.get(a).add(new Edge(b, c));
-			adj.get(b).add(new Edge(a, c));
-		}
-
-		for (int i = 0; i < n; i++) {
-			out.print(i + " IS CONNECTED TO: ");
-			for (Edge j : adj.get(i))
-				out.print(j.dest + " ");
-			out.println();
-		}
+			for (int j = 0; j < n; j++)
+				m[i][j] = readInt();
 		out.close();
-	}
-
-	static class Edge {
-		int dest, cost;
-		Edge (int dest, int cost) {
-			this.dest = dest;
-			this.cost = cost;
-		}
 	}
 
 	static String next () throws IOException {
@@ -84,3 +53,4 @@ public class AdjacencyList {
 		return br.readLine().trim();
 	}
 }
+
