b0f1ce7f87dd357492afc1b45adf34905c1047fe
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-09-26 09:33:48 -0400

    Updated

--- src/codebook/graph/network/MinCostMaxMatchingHungarian.java
+++ src/codebook/graph/network/MinCostMaxMatchingHungarian.java
@@ -19,7 +19,7 @@ public class MinCostMaxMatchingHungarian {
 	public static void main (String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		out = new PrintWriter(new OutputStreamWriter(System.out));
-		//br = new BufferedReader(new FileReader("in.txt"));
+		//br = new BufferedReader	(new FileReader("in.txt"));
 		//out = new PrintWriter(new FileWriter("out.txt"));
 
 		// precondition: a square matrix where the rows represent workers and the columns represents jobs
