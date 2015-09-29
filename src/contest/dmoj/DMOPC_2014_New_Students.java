f73f40ea24a27bf5c41ad0f7022e40a8055eae3e
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-09-28 23:02:29 -0400

    Added DMOPC New Students

---
+++ src/contest/dmoj/DMOPC_2014_New_Students.java
@@ -0,0 +1,57 @@
+package contest.dmoj;
+
+import java.util.*;
+import java.io.*;
+
+public class DMOPC_2014_New_Students {
+
+	static BufferedReader br;
+	static PrintWriter out;
+	static StringTokenizer st;
+
+	public static void main (String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		//br = new BufferedReader(new FileReader("in.txt"));
+		//out = new PrintWriter(new FileWriter("out.txt"));
+
+		int n = readInt();
+		int sum = 0;
+		for (int i = 0; i < n; i++)
+			sum += readInt();
+		int m = readInt();
+		for (int i = 0; i < m; i++) {
+			sum += readInt();
+			out.printf("%.3f\n", sum / (double)(n + i + 1));
+		}
+		
+		out.close();
+	}
+
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
+
