d4d400b672824ec0a8983396824a9f29a7a7fd41
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-09-29 22:59:47 -0400

    Added DMOPC Tiles

---
+++ src/contest/dmoj/DMOPC_2014_Tiles.java
@@ -0,0 +1,52 @@
+package contest.dmoj;
+
+import java.util.*;
+import java.io.*;
+
+public class DMOPC_2014_Tiles {
+
+	static BufferedReader br;
+	static PrintWriter out;
+	static StringTokenizer st;
+
+	public static void main (String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		//br = new BufferedReader(new FileReader("in.txt"));
+		//out = new PrintWriter(new FileWriter("out.txt"));
+
+		int n = readInt();
+		int m = readInt();
+		int s = readInt();
+		out.println((n/s) * (m/s));
+		
+		out.close();
+	}
+
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
+
