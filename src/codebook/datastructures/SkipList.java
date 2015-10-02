406302b7f7f35929b794f6038f86018678a24756
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-10-02 11:18:14 -0400

    Update SkipList.java

--- src/codebook/datastructures/SkipList.java
+++ src/codebook/datastructures/SkipList.java
@@ -3,21 +3,48 @@ class SkipList {
 	private int maxLevel;
 	
 	SkipList () {
-		head = new Node(null);
-		maxLevel = 0;
+		this.head = new Node(null);
+		this.maxLevel = 0;
 	}
 	
 	public void add (Integer val) {
-		
+		int newLevel = (int)(Math.random() * (maxLevel + 1));
+		if (newLevel > maxLevel) {
+			maxLevel++;
+			Node newNode = new Node(null);
+			newNode.down = head;
+			head = newNode;
+		}
+		Node currentNode = head;
+		for (int i = maxLevel; i >= 0; i--) {
+			currentNode = add(currentNode, val, i, newLevel);
+		}
 	}
-	
+	private Node add (Node current, Integer val, int currentLevel, int wantedLevel) {
+		while (current != null && current.val < val)
+			current = current.right;
+		if (currentLevel <= wantedLevel) {
+			Node newNode = new Node(val);
+			newNode.right = current;
+			current = newNode;
+		}
+		if (currentLevel == 0)
+			return current;
+		current.down = add(current.down, val, currentLevel - 1, wantedLevel);
+		return current;
+	}
 	static class Node {
 		Integer val;
-		LinkedList<Node> next;
+		Node down, right;
 		Node (Integer val) {
 			this.val = val;
-			this.next = new LinkedList<Node>();
+			this.down = null;
+			this.right = null;
 		}
 	}	
+	public static void main (String[] args) {
+		SkipList list = new SkipList();
+		
+	}
 	
 }
