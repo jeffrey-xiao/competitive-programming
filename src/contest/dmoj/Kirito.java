08a598b9e26382a840839370ee5fb3e32f1acc54
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-10-07 12:32:18 -0400

    Initial Commit

---
+++ src/contest/dmoj/Kirito.java
@@ -0,0 +1,126 @@
+package contest.dmoj;
+
+import java.util.*;
+import java.io.*;
+
+public class Kirito {
+
+	static BufferedReader br;
+	static PrintWriter out;
+	static StringTokenizer st;
+
+	static int[] tree, lcp;
+	static String[] grid;
+	static int n, m;
+	
+	public static void main (String[] args) throws IOException {
+		//br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		br = new BufferedReader(new FileReader("in.txt"));
+		//out = new PrintWriter(new FileWriter("out.txt"));
+
+		n = readInt();
+		m = readInt();
+		tree = new int[n*3];
+		grid = new String[n+1];
+		for (int i = 1; i <= n; i++)
+			grid[i] = next();
+		lcp = new int[n+1];
+		for (int i = 2; i <= n; i++) {
+			int j = 0;
+			for (; j < m; j++)
+				if (grid[i].charAt(j) != grid[i-1].charAt(j))
+					break;
+			lcp[i] = j;
+		}
+		build(1, 1, n);
+		int q = readInt();
+		for (int i = 0; i < q; i++) {
+			int l = readInt() ;
+			int r = readInt();
+			if (l == r)
+				out.println(m * (r - l + 1));
+			else {
+				out.println(query(1, 1, n, l + 1, r) * (r - l + 1));
+				String temp = grid[l];
+				grid[l] = grid[r];
+				grid[r] = temp;
+				update(l);
+				update(l+1);
+				update(r);
+				update(r+1);
+			}
+		}
+		out.close();
+	}
+	static void update (int i) {
+		if (i <= 1 || i > n)
+			return;
+		int j = 0;
+		for (; j < m; j++)
+			if (grid[i].charAt(j) != grid[i-1].charAt(j))
+				break;
+		lcp[i] = j;
+		update(1, 1, n, i);
+	}
+	static void update (int n, int l, int r, int x) {
+		if (x == l && x == r) {
+			tree[n] = lcp[x];
+			return;
+		}
+		int mid = (l + r) >> 1;
+		if (x <= mid)
+			update(n << 1, l, mid, x);
+		else
+			update(n << 1 | 1, mid+1, r, x);
+		tree[n] = Math.min(tree[n<<1], tree[n<<1|1]);
+	}
+	static void build (int n, int l, int r) {
+		if (l == r) {
+			tree[n] = lcp[l];
+			return;
+		}
+		int mid = (l + r) >> 1;
+		build(n << 1, l, mid);
+		build(n << 1 | 1, mid+1, r);
+		tree[n] = Math.min(tree[n << 1], tree[n << 1 | 1]);
+	}
+	static int query (int n, int l, int r, int ql, int qr) {
+		if (l == ql && r == qr) {
+			return tree[n];
+		}
+		int mid = (l + r) >> 1;
+		if (qr <= mid)
+			return query(n << 1, l, mid, ql, qr);
+		else if (ql > mid)
+			return query(n << 1 | 1, mid+1, r, ql, qr);
+		else
+			return Math.min(query(n << 1, l, mid, ql, mid), query(n << 1 | 1, mid+1, r, mid+1, qr));
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
+
---
+++ src/contest/dmoj/VMSS_Jeffrey_And_Frank_And_A_Lack_Of_Roads.java
@@ -0,0 +1,106 @@
+package contest.dmoj;
+
+import java.util.*;
+import java.io.*;
+
+public class VMSS_Jeffrey_And_Frank_And_A_Lack_Of_Roads {
+
+	static BufferedReader br;
+	static PrintWriter out;
+	static StringTokenizer st;
+
+	public static void main (String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		//br = new BufferedReader(new FileReader("in.txt"));
+		//out = new PrintWriter(new FileWriter("out.txt"));
+
+		int n = readInt();
+		int r = readInt();
+		int s = readInt();
+		int[][] dp = new int[r+1][s+1];
+		
+		Apple[] a = new Apple[n];
+		for (int i = 0; i < n; i++)
+			a[i] = new Apple(next(), readInt(), readInt(), readInt());
+		Arrays.sort(a);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= r - a[i].cost; j++) {
+				for (int k = 0; k <= s - a[i].volume; k++) {
+					dp[j + a[i].cost][k + a[i].volume] = Math.max(dp[j][k] + a[i].value, dp[j + a[i].cost][k + a[i].volume]);
+				}
+			}
+//			for (int j = 0; j <= r; j++) {
+//				for (int k = 0; k <= s; k++) {
+//					System.out.print(dp[i][j] + " ");
+//				}
+//				System.out.println();
+//			}
+//			System.out.println();
+//			System.out.println();
+		}
+		int currR = r;
+		int currS = s;
+		int[] cnt = new int[n];
+		boolean found = true;
+		while (found) {
+			found = false;
+			for (int i = 0; i < n; i++) {
+				if (currR - a[i].cost >= 0 && currS - a[i].volume >= 0 && dp[currR][currS] == dp[currR - a[i].cost][currS - a[i].volume] + a[i].value) {
+					currR -= a[i].cost;
+					currS -= a[i].volume;
+					cnt[i]++;
+					found = true;
+					break;
+				}
+			}
+		}
+		out.println(dp[r][s]);
+		for (int i = 0; i < n; i++)
+//			if (cnt[i] > 0)
+				out.println(a[i].name + " " + cnt[i]);
+		out.close();
+	}
+
+	static class Apple implements Comparable<Apple> {
+		String name;
+		int value, cost, volume;
+		Apple (String name, int value, int cost, int volume) {
+			this.name = name;
+			this.value = value;
+			this.cost = cost;
+			this.volume = volume;
+		}
+		@Override
+		public int compareTo (Apple a) {
+			return name.compareTo(a.name);
+		}
+	}
+	
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
+
