171c01fc3d5fcb1a8b57460cd95faee7a399e1a5
jeffrey-xiao <jeffrey.xiao1998@gmail.com>
2015-10-29 20:24:28 -0400

    Initial Commit: Woburn 2015 October Junior_

---
+++ src/contest/woburn/Woburn_Challenge_2015_Do_You_Know_Your_Woburn_Colors.java
@@ -0,0 +1,57 @@
+package contest.woburn;
+
+import java.util.*;
+import java.io.*;
+
+public class Woburn_Challenge_2015_Do_You_Know_Your_Woburn_Colors {
+
+	static BufferedReader br;
+	static PrintWriter out;
+	static StringTokenizer st;
+
+	public static void main (String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		//br = new BufferedReader(new FileReader("in.txt"));
+		//out = new PrintWriter(new FileWriter("out.txt"));
+
+		String str1 = readLine();
+		String str2 = readLine();
+
+		if (!str1.equals("RED") && !str2.equals("RED"))
+			out.println("RED");
+		else if (!str1.equals("BLUE") && !str2.equals("BLUE"))
+			out.println("BLUE");
+		else if (!str1.equals("WHITE") && !str2.equals("WHITE"))
+			out.println("WHITE");
+
+		out.close();
+	}
+
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
+
---
+++ src/contest/woburn/Woburn_Challenge_2015_Trip_Budgeting.java
@@ -0,0 +1,77 @@
+package contest.woburn;
+
+import java.util.*;
+import java.io.*;
+
+public class Woburn_Challenge_2015_Trip_Budgeting {
+
+	static BufferedReader br;
+	static PrintWriter out;
+	static StringTokenizer st;
+
+	public static void main (String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		//br = new BufferedReader(new FileReader("in.txt"));
+		//out = new PrintWriter(new FileWriter("out.txt"));
+
+		int emin = readInt();
+		int tbase = readInt();
+		int hbase = readInt();
+		int fbase = readInt();
+		
+		int n = readInt();
+		int[] e = new int[n], t = new int[n], h = new int[n], f = new int[n];
+		for (int i = 0; i < n; i++) {
+			e[i] = readInt();
+			t[i] = readInt();
+			h[i] = readInt();
+			f[i] = readInt();
+		}
+		int ans = 1 << 30;
+		for (int i = 0; i < 1 << n; i++) {
+			int ecurr = 0;
+			int tcurr = tbase;
+			int hcurr = hbase;
+			int fcurr = fbase;
+			for (int j = 0; j < n; j++)
+				if ((i & 1 << j) > 0) {
+					ecurr += e[j];
+					tcurr += t[j];
+					hcurr = Math.max(hcurr, h[j]);
+					fcurr = Math.max(0, fcurr - f[j]);
+				}
+			if (ecurr >= emin)
+				ans = Math.min(ans, tcurr + hcurr + fcurr);
+		}
+		out.println(ans);
+		out.close();
+	}
+
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
+
