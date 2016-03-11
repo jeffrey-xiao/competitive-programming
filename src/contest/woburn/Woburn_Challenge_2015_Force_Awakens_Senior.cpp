4023145b2d4a06eeff717f639d85c8e5b66e3f43
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-03-10 23:21:24 -0500

    Added Woburn Challenge

---
+++ src/contest/woburn/Woburn_Challenge_2015_Force_Awakens_Senior.cpp
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+
+#define mp make_pair
+#define pb push_back
+#define INF 1<<30
+#define MOD 1000000007
+#define EPS 1e-6
+#define rint(x) scanf("%d", &(x))
+#define rlong(x) scanf("%lld", &(x))
+#define SIZE 1600000
+#define l(x) x << 1
+#define r(x) x << 1|1
+#define m(x, y) (x + y)/2
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef pair<double, int> pdi;
+
+int N, M;
+double X1, Y1, X2, Y2;
+ll lazy[SIZE], damage[SIZE], ships[SIZE];
+
+priority_queue<pdi, vector<pdi>, greater<pdi>> pq;
+static void pushUp (int n) {
+    // damage[n] = damage[n << 1] + damage[n << 1 | 1];
+    ships[n] = ships[n << 1] + ships[n << 1 | 1];
+}
+
+void pushDown (int n) {
+    if (lazy[n] > 0) {
+        lazy[n << 1] += lazy[n];
+        lazy[n << 1 | 1] += lazy[n];
+        damage[n << 1] += lazy[n] * ships[n << 1];
+        damage[n << 1 | 1] += lazy[n] * ships[n << 1 | 1];
+        lazy[n] = 0;
+    }
+}
+
+ll query (int n, int l, int r, int ql, int qr) {
+    if (l == ql && r == qr)
+        return damage[n];
+    pushDown(n);
+
+    int mid = (l + r) >> 1;
+
+    if (qr <= mid)
+        return query(n << 1, l, mid, ql, qr);
+    else if (ql > mid)
+        return query(n << 1 | 1, mid + 1, r, ql, qr);
+    return query(n << 1, l, mid, ql, mid) + query(n << 1 | 1, mid + 1, r, mid + 1, qr);
+}
+
+void updateDamage (ll c) {
+    lazy[1] += c;
+    damage[1] += c * ships[1];
+}
+
+void update (int n, int l, int r, int x) {
+    if (l == abs(x) && abs(x) == r) {
+        if (x < 0)
+            ships[n] = 0;
+        else
+            ships[n] = 1;
+        return;
+    }
+
+    pushDown(n);
+
+    int mid = (l + r) >> 1;
+
+    if (abs(x) <= mid)
+        update(n << 1, l, mid, x);
+    else
+        update(n << 1 | 1, mid + 1, r, x);
+
+    pushUp(n);
+}
+inline bool between(double x, double l, double h) {
+    return l - EPS < x && x < h + EPS;
+}
+inline double dist(double x, double y) {
+    return sqrt(x * x + y * y);
+}
+
+int main () {
+
+    rint(N), rint(M);
+    scanf("%lf%lf%lf%lf", &X1, &Y1, &X2, &Y2);
+
+
+    for (int i = 1; i <= N; i++) {
+        double x, y, dx, dy;
+        scanf("%lf%lf%lf%lf", &x, &y, &dx, &dy);
+
+        vector<double> events;
+
+        events.clear();
+        if (dx == 0) {
+            if (x < X1 || x > X2) continue;
+            events.push_back(Y1 - y);
+            events.push_back(Y2 - y);
+            if (dy < 0) {
+                for (int j = 0; j < (int)events.size(); j++)
+                    events[j] = -events[j];
+            }
+        } else {
+            double m = (double)dy / dx;
+            double px = -1E6, py = y - m * (x + 1E6);
+            double d = dist(x - px, y - py);
+            y = py + m * (X1 - px);
+            if (between(y, Y1, Y2)) events.push_back(dist(X1 - px, y - py) - d);
+            y = py + m * (X2 - px);
+            if (between(y, Y1, Y2)) events.push_back(dist(X2 - px, y - py) - d);
+            if (dy != 0) {
+                x = (m * px + Y1 - py) / m;
+                if (between(x, X1, X2)) events.push_back(dist(x - px, Y1 - py) - d);
+                x = (m * px + Y2 - py) / m;
+                if (between(x, X1, X2)) events.push_back(dist(x - px, Y2 - py) - d);
+            }
+            if (dx < 0) {
+                for (int j = 0; j < (int)events.size(); j++)
+                    events[j] = -events[j];
+            }
+        }
+        if (events.size() >= 2) {
+            sort(events.begin(), events.end());
+            pq.push(make_pair(events[0] / dist(dx, dy) - EPS, i));
+            pq.push(make_pair(events.back() / dist(dx, dy) + EPS, -i));
+        }
+    }
+    int time = 0;
+    for (int i = 0; i < M; i++) {
+        int a, b, c;
+        rint(a), rint(b), rint(c);
+
+        if (a == 1) {
+            time += b;
+            while (!pq.empty() && pq.top().first <= time) {
+                pdi e = pq.top();
+                pq.pop();
+                update(1, 1, N, e.second);
+            }
+            updateDamage(c);
+        } else {
+            if (b <= c) {
+                printf("%lld\n", query(1, 1, N, b, c));
+            } else {
+                printf("0\n");
+            }
+        }
+    }
+    return 0;
+}
---
+++ src/contest/woburn/Woburn_Challenge_2015_Lex_Luthors_Landmines.cpp
@@ -0,0 +1,278 @@
+#include <bits/stdc++.h>
+
+#define mp make_pair
+#define pb push_back
+#define INF 1<<30
+#define MOD 1000000007
+#define rint(x) scanf("%d", &(x))
+#define rlong(x) scanf("%lld", &(x))
+#define SIZE 1000010
+#define l(x) x << 1
+#define r(x) x << 1|1
+#define m(x, y) (x + y)/2
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+
+using namespace std;
+
+char _;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+struct mine {
+    int pos, l, r, index, cnt;
+    mine (int pos, int l, int r, int cnt) {
+        this->pos = pos;
+        this->l = l;
+        this->r = r;
+        this->cnt = cnt;
+    }
+    mine () {}
+};
+
+int N, M;
+map<int, mine> mineMap;
+mine mines[SIZE];
+
+// seg tree
+int minIndex[4 * SIZE], maxIndex[4 * SIZE];
+
+vector<vector<int>> adj (SIZE);
+
+// SCC
+int lo[SIZE], disc[SIZE], id[SIZE], sz[SIZE];
+int cnt = 0, comCnt = 0;
+deque<int> q;
+
+// condensed graph
+vector<unordered_set<int>> g (SIZE);
+bool vis[SIZE], inStack[SIZE];
+int lRange[SIZE], rRange[SIZE];
+
+int ans[SIZE];
+
+void order (int u) {
+    vis[u] = true;
+    for (int v : g[u])
+        if (!vis[v])
+            order(v);
+
+    q.push_back(u);
+}
+
+void dfs (int u) {
+    disc[u] = lo[u] = ++cnt;
+    q.push_back(u);
+    inStack[u] = true;
+
+    for (int v : adj[u]) {
+        if (disc[v] == 0) {
+            dfs(v);
+            lo[u] = min(lo[u], lo[v]);
+        } else if (inStack[v]) {
+            lo[u] = min(lo[u], disc[v]);
+        }
+    }
+
+    if (disc[u] == lo[u]) {
+        while (q.back() != u) {
+            inStack[q.back()] = false;
+            sz[comCnt] += mines[q.back()].cnt;
+            id[q.back()] = comCnt;
+            q.pop_back();
+        }
+        inStack[q.back()] = false;
+        sz[comCnt] += mines[q.back()].cnt;
+        id[q.back()] = comCnt;
+        q.pop_back();
+
+        comCnt++;
+    }
+}
+
+int queryMin (int n, int l, int r, int ql, int qr) {
+    if (l == ql && r == qr) {
+        return minIndex[n];
+    }
+
+    int mid = (l + r) >> 1;
+
+    if (qr <= mid) {
+        return queryMin(n << 1, l, mid, ql, qr);
+    } else if (ql > mid) {
+        return queryMin(n << 1 | 1, mid + 1, r, ql, qr);
+    } else {
+        int index1 = queryMin(n << 1 | 1, mid + 1, r, mid + 1, qr);
+        int index2 = queryMin(n << 1, l, mid, ql, mid);
+        if (mines[index1].l <= mines[index2].l)
+            return index1;
+        else
+            return index2;
+    }
+}
+
+int queryMax (int n, int l, int r, int ql, int qr) {
+    if (l == ql && r == qr) {
+        return maxIndex[n];
+    }
+
+    int mid = (l + r) >> 1;
+
+    if (qr <= mid) {
+        return queryMax(n << 1, l, mid, ql, qr);
+    } else if (ql > mid) {
+        return queryMax(n << 1 | 1, mid + 1, r, ql, qr);
+    } else {
+        int index1 = queryMax(n << 1 | 1, mid + 1, r, mid + 1, qr);
+        int index2 = queryMax(n << 1, l, mid, ql, mid);
+        if (mines[index1].r >= mines[index2].r)
+            return index1;
+        else
+            return index2;
+    }
+}
+
+void build (int n, int l, int r) {
+    if (l == r) {
+        minIndex[n] = l;
+        maxIndex[n] = l;
+        return;
+    }
+
+    int mid = (l + r) >> 1;
+
+    build(n << 1, l, mid);
+    build(n << 1 | 1, mid + 1, r);
+
+    if (mines[minIndex[n << 1]].l < mines[minIndex[n << 1 | 1]].l) {
+        minIndex[n] = minIndex[n << 1];
+    } else {
+        minIndex[n] = minIndex[n << 1 | 1];
+    }
+
+    if (mines[maxIndex[n << 1]].r > mines[maxIndex[n << 1 | 1]].r) {
+        maxIndex[n] = maxIndex[n << 1];
+    } else {
+        maxIndex[n] = maxIndex[n << 1 | 1];
+    }
+}
+
+int main () {
+	// freopen("in.txt", "r", stdin);
+	// freopen("out.txt", "w", stdout);
+
+    scan(N);
+    scan(M);
+
+    for (int i = 0; i < N; i++) {
+        int pos, l, r;
+        scan(pos); scan(l); scan(r);
+
+        if (!mineMap.count(pos)) {
+            mine m = mine(pos, INF, -INF, 0);
+            mineMap[pos] = m;
+        }
+        mine old = mineMap[pos];
+        mineMap[pos] = mine(pos, min(old.l, pos - l), max(old.r, pos + r), old.cnt + 1);
+    }
+
+    N = mineMap.size();
+
+    int cnt = 1;
+
+    for (pair<int, mine> e : mineMap) {
+        mine m = e.second;
+        m.index = cnt;
+        e.second.index = cnt;
+        mines[cnt++] = m;
+    }
+
+    for (int i = 1; i <= N; i++)
+        mineMap[mines[i].pos].index = i;
+
+    build(1, 1, N);
+
+    for (int i = 1; i <= N; i++) {
+        auto lower = mineMap.lower_bound(mines[i].l);
+
+        auto higher = mineMap.lower_bound(mines[i].r);
+        if (higher->first != mines[i].r)
+            --higher;
+        if (lower != mineMap.end() && higher != mineMap.begin()) {
+            int lowerIndex = lower->second.index;
+            int higherIndex = higher->second.index;
+
+            int lo = queryMin(1, 1, N, lowerIndex, higherIndex);
+            int hi = queryMax(1, 1, N, lowerIndex, higherIndex);
+
+            if (lo != i)
+                adj[i].pb(lo);
+
+            if (hi != i)
+                adj[i].pb(hi);
+        }
+    }
+
+    for (int i = 1; i <= N; i++)
+        if (disc[i] == 0)
+            dfs(i);
+
+    for (int i = 0; i < comCnt; i++) {
+        lRange[i] = INF;
+        rRange[i] = -INF;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        lRange[id[i]] = min(lRange[id[i]], mines[i].l);
+        rRange[id[i]] = max(rRange[id[i]], mines[i].r);
+    }
+
+    for (int i = 1; i <= N; i++)
+        for (int j : adj[i])
+            if (id[i] != id[j])
+                g[id[i]].insert(id[j]);
+
+    for (int i = 0; i < comCnt; i++)
+        if (!vis[i])
+            order(i);
+
+    for (int u : q) {
+        for (int v : g[u]) {
+            lRange[u] = min(lRange[u], lRange[v]);
+            rRange[u] = max(rRange[u], rRange[v]);
+        }
+    }
+
+    priority_queue<pair<int, pi>, vector<pair<int, pi>>, greater<pair<int, pi>>> pq;
+
+    for (int i = 0; i < comCnt; i++) {
+        pq.push(mp(lRange[i], mp(-2, sz[i])));
+        pq.push(mp(rRange[i] + 1, mp(-1, sz[i])));
+    }
+
+    for (int i = 0; i < M; i++) {
+        int val;
+        scan(val);
+        pq.push(mp(val, mp(i, 0)));
+    }
+
+    int curr = 0;
+
+    while (!pq.empty()) {
+        pair<int, pi> currState = pq.top();
+        pq.pop();
+        if (currState.second.first == -2) {
+            curr += currState.second.second;
+        } else if (currState.second.first == -1) {
+            curr -= currState.second.second;
+        } else {
+            ans[currState.second.first] = curr;
+        }
+    }
+
+    for (int i = 0; i < M; i++)
+        printf("%d\n", ans[i]);
+
+	return 0;
+}
