dc09bf3dc3b7e69c82da10b78f59029bbbfabaa4
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-05-08 19:51:49 -0400

    Updated binary search

--- src/codebook/algorithms/BinarySearch.java
+++ src/codebook/algorithms/BinarySearch.java
@@ -7,9 +7,9 @@
 package codebook.algorithms;
 
 public class BinarySearch {
-	public int binarySearch (int[] a, int lo, int hi, int val) {
+	public static int bsearch (int[] a, int lo, int hi, int val) {
 		while (lo <= hi) {
-			int mid = lo + (hi - lo) >> 1;
+			int mid = lo + ((hi - lo) >> 1);
 			if (a[mid] == val)
 				return mid;
 			else if (a[mid] < val)
@@ -18,5 +18,33 @@ public class BinarySearch {
 				hi = mid - 1;
 		}
 		return -1;
+	}
+	
+	public static int bsearchCeil (int[] a, int lo, int hi, int val) {
+		while (lo <= hi) {
+			int mid = lo + ((hi - lo) >> 1);
+			if (a[mid] < val)
+				lo = mid + 1;
+			else
+				hi = mid - 1;
+		}
+		return lo;
+	}
+	
+	public static int bsearchFloor (int[] a, int lo, int hi, int val) {
+		while (lo <= hi) {
+			int mid = lo + ((hi - lo) >> 1);
+			if (a[mid] <= val)
+				lo = mid + 1;
+			else
+				hi = mid - 1;
+		}
+		return hi;
+	}
+	
+	public static void main (String[] args) {
+		int[] a = {1, 2, 3, 4, 4, 4, 4, 4, 4, 6, 7, 8};
+		System.out.println(bsearchCeil(a, 0, 11, 9));
+		System.out.println(bsearchFloor(a, 0, 11, 4));
 	}
 }
