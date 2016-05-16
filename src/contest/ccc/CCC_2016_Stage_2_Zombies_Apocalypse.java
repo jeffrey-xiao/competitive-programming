6cd8cc9fe5c897e119c985db256683343bdf4fd2
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-05-16 17:08:22 -0400

    Finished CCO 2016 Zombie Apocalypse

--- src/contest/ccc/CCC_2016_Stage_2_Zombies_Apocalypse.java
+++ src/contest/ccc/CCC_2016_Stage_2_Zombies_Apocalypse.java
@@ -9,36 +9,105 @@ public class CCC_2016_Stage_2_Zombies_Apocalypse {
 	static PrintWriter out;
 	static StringTokenizer st;
 
-	static int N, M, K;
-	
+	static int N, M, K, Q;
+	static int[] R, C;
 	public static void main (String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		out = new PrintWriter(new OutputStreamWriter(System.out));
 		//br = new BufferedReader(new FileReader("in.txt"));
 		//out = new PrintWriter(new FileWriter("out.txt"));
 
+		N = readInt();
+		M = readInt();
+		K = readInt();
+		
+		R = new int[K];
+		C = new int[K];
+		
+		for (int i = 0; i < K; i++) {
+			R[i] = readInt();
+			C[i] = readInt();
+		}
+
+		Q = readInt();
+		
+		out.println(getArea(Q) - getArea(Q - 1	));
+		
 		out.close();
 	}
 
-	static class Line implements Comparable<Line> {
-		int c, lor, hir, index, type;
+	static long getArea (int q) {
+		Point[] ver = new Point[2 * K];
+		Point[] hor = new Point[2 * K];
+		boolean[] use = new boolean[K];
 		
-		Line (int c, int lor, int hir, int index, int type) {
-			this.c = c;
-			this.lor = lor;
-			this.hir = hir;
+		for (int i = 0; i < K; i++) {
+			int loR = Math.max(1, R[i] - q);
+			int hiR = Math.min(N, R[i] + q);
+			int loC = Math.max(1, C[i] - q);
+			int hiC = Math.min(M, C[i] + q);
+			hor[i << 1] = new Point(loC, i, 1);
+			hor[i << 1 | 1] = new Point(hiC + 1, i, -1);
+			ver[i << 1] = new Point(loR, i, 1);
+			ver[i << 1 | 1] = new Point(hiR + 1, i, -1);
+		}
+		
+		Arrays.sort(ver);
+		Arrays.sort(hor);
+		
+		// area of sweepline 
+		long horRet = 0;
+		// previous horizontal position
+		long horPrev = -1;
+		for (int i = 0; i < 2 * K; i++) {
+			// previous vertical position
+			long verPrev = -1;
+			// area of vertical strip
+			long verRet = 0;
+			// height of overlapping strips
+			long curr = 0;
+			for (int j = 0; j < 2 * K; j++) {
+				verRet += (ver[j].pos - verPrev) * Math.min(curr, 1);
+				verPrev = ver[j].pos;
+				if (use[ver[j].index])
+					curr += ver[j].type;
+				
+				while (j + 1 < 2 * K && ver[j + 1].pos == verPrev) {
+					j++;
+					if (use[ver[j].index])
+						curr += ver[j].type;
+				}
+			}
+			
+			horRet += (hor[i].pos - horPrev) * verRet;
+			
+			horPrev = hor[i].pos;
+			
+			use[hor[i].index] = hor[i].type == 1 ? true : false;
+			
+			while (i + 1 < 2 * K && hor[i + 1].pos == horPrev) {
+				i++;
+				use[hor[i].index] = hor[i].type == 1 ? true : false;
+			}
+		}
+		
+		return horRet;
+	}
+	
+	static class Point implements Comparable<Point> {
+		Long pos;
+		int index, type;
+		
+		Point (long pos, int index, int type) {
+			this.pos = pos;
 			this.index = index;
 			this.type = type;
 		}
 
 		@Override
-		public int compareTo (Line l) {
-			return c - l.c;
+		public int compareTo (Point o) {
+			return pos.compareTo(o.pos);
 		}
-	}
-	
-	static class Point implements Comparable<Point> {
-		int r, index, type;
 	}
 	
 	static String next () throws IOException {
