724851bc1586327bb263ffd3a73bbc5b77197573
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-05-23 22:30:24 -0400

    Fixed dinic's

--- src/codebook/graph/network/MaxFlowDinic.java
+++ src/codebook/graph/network/MaxFlowDinic.java
@@ -12,6 +12,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -23,7 +24,7 @@ public class MaxFlowDinic {
 
 	static Edge[] e;
 	static int[] last, dist;
-	static int n, m, cnt, src, sink;
+	static int N, M, cnt, src, sink;
 
 	public static void main (String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,19 +32,18 @@ public class MaxFlowDinic {
 		//br = new BufferedReader(new FileReader("in.txt"));
 		//out = new PrintWriter(new FileWriter("out.txt"));
 
-		n = readInt();
-		m = readInt();
+		N = readInt();
+		M = readInt();
 
-		src = readInt();
-		sink = readInt();
+		src = 0;
+		sink = N - 1;
 
-		last = new int[n];
-		e = new Edge[2 * m];
+		last = new int[N];
+		e = new Edge[2 * M];
 
-		for (int i = 0; i < n; i++)
-			last[i] = -1;
-
-		for (int i = 0; i < m; i++) {
+		Arrays.fill(last, -1);
+		
+		for (int i = 0; i < M; i++) {
 			int a = readInt() - 1;
 			int b = readInt() - 1;
 			int c = readInt();
@@ -54,22 +54,23 @@ public class MaxFlowDinic {
 		out.close();
 	}
 
-	static int getFlow () {
-		int res = 0;
-		int curr = 0;
+	static long getFlow () {
+		long res = 0;
+
 		while (getPath())
-			while ((curr = dfs(0, 1 << 30)) > 0)
-				res += curr;
+			res += dfs(src, 1 << 30);
+
 		return res;
 	}
 
 	static boolean getPath () {
-		dist = new int[n];
-		for (int i = 0; i < n; i++)
-			dist[i] = -1;
+		dist = new int[N];
+		Arrays.fill(dist, -1);
+
 		Queue<Integer> q = new ArrayDeque<Integer>();
 		q.offer(src);
 		dist[src] = 0;
+
 		while (!q.isEmpty()) {
 			int curr = q.poll();
 			for (int i = last[curr]; i != -1; i = e[i].next) {
@@ -79,23 +80,29 @@ public class MaxFlowDinic {
 				}
 			}
 		}
-		return dist[sink - 1] != -1;
+
+		return dist[sink] != -1;
 	}
 
-	static int dfs (int curr, int flow) {
-		if (curr == sink - 1)
+	static long dfs (int curr, long flow) {
+		if (curr == sink)
 			return flow;
+
+		long ret = 0;
+
 		for (int i = last[curr]; i != -1; i = e[i].next) {
 			if (e[i].cost > 0 && dist[e[i].dest] == dist[curr] + 1) {
-				int res = dfs(e[i].dest, Math.min(flow, e[i].cost));
-				if (res > 0) {
-					e[i].cost -= res;
-					e[i ^ 1].cost += res;
-					return res;
-				}
+				long res = dfs(e[i].dest, Math.min(flow, e[i].cost));
+				ret += res;
+				e[i].cost -= res;
+				e[i ^ 1].cost += res;
+				flow -= res;
+
+				if (flow == 0)
+					break;
 			}
 		}
-		return 0;
+		return ret;
 	}
 
 	static void addEdge (int x, int y, int xy, int yx) {
@@ -106,9 +113,10 @@ public class MaxFlowDinic {
 	}
 
 	static class Edge {
-		int dest, cost, next;
+		int dest, next;
+		long cost;
 
-		Edge (int dest, int cost, int next) {
+		Edge (int dest, long cost, int next) {
 			this.dest = dest;
 			this.cost = cost;
 			this.next = next;
