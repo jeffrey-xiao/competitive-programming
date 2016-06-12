32e05380f61f7b2ac3dac1a7981d2d5d2eb1ffc0
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-06-12 19:02:00 -0400

    Added COCI 2013 Contest 1 solutions

---
+++ src/contest/coci/COCI_2013_ORGANIZATOR.java
@@ -0,0 +1,67 @@
+package contest.coci;
+
+import java.util.*;
+import java.io.*;
+
+public class COCI_2013_ORGANIZATOR {
+
+	static BufferedReader br;
+	static PrintWriter out;
+	static StringTokenizer st;
+
+	static final int MAX_SIZE = 2000000;
+	
+	static int N;
+	static int[] occ = new int[MAX_SIZE + 1];
+	
+	public static void main (String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		//br = new BufferedReader(new FileReader("in.txt"));
+		//out = new PrintWriter(new FileWriter("out.txt"));
+
+		N = readInt();
+		
+		for (int i = 0; i < N; i++)
+			occ[readInt()]++;
+		
+		long ans = 0;
+		for (int i = 1; i <= MAX_SIZE; i++) {
+			long curr = 0;
+			for (int j = 1; i * j <= MAX_SIZE; j++)
+				curr += occ[j * i];
+			if (curr > 1)
+				ans = Math.max(ans, curr * i);
+		}
+		
+		out.println(ans);
+		out.close();
+	}
+
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
+
