f2c2700aafd8d849a42f5e147d5b8096b937c59a
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-06-13 19:01:33 -0400

    Updated incorrect/incomplete solution

--- src/contest/coci/COCI_2007_PRAVOKUTNI.java
+++ src/contest/coci/COCI_2007_PRAVOKUTNI.java
@@ -1,104 +1,109 @@
 package contest.coci;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class COCI_2007_PRAVOKUTNI {
 
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static PrintWriter ps = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+	static BufferedReader br;
+	static PrintWriter out;
 	static StringTokenizer st;
-
+	
 	static int N;
-
+	static Point[] p;
+	
 	public static void main (String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		//br = new BufferedReader(new FileReader("in.txt"));
+		//out = new PrintWriter(new FileWriter("out.txt"));
+		
 		N = readInt();
-		Point[] p = new Point[N];
-		Point[] sort = new Point[N];
-		for (int i = 0; i < N; i++) {
-			int x = readInt(), y = readInt();
-			p[i] = new Point(x, y);
-			sort[i] = new Point(x, y);
-		}
-		int res = 0;
-		for (int i = 0; i < N; i++) {
-			curr = p[i];
-			Arrays.sort(sort);
-			System.out.println();
-			System.out.printf("Current x: %d; y: %d\n", p[i].x, p[i].y);
-			// System.out.println(angle(curr, sort[j]));
-			// }
-			int lo = 1;
-			int hi = 2;
-			while (hi <= N) {
-				if (lo == hi) {
-					hi++;
-					continue;
-				}
-				int i1 = lo, i2 = hi == N ? 1 : hi;
-				double angle = angle(sort[i1], curr) - angle(sort[i1], sort[i2]);
-				// System.out.println(angle(c, curr) + " " + angle(c,
-				// sort[i2]));
-				System.out.println(curr + " " + sort[i1] + " " + sort[i2]);
-				boolean right = true;
-				if (angle < 0) {
-					angle += 360;
-					right = false;
-				}
-				if (angle > 90) {
-					if (right)
-						hi++;
-					else
-						hi++;
-				} else if (angle < 90) {
-					if (right)
-						lo++;
-					else
-						lo++;
-				} else {
-					hi++;
-					res++;
-				}
-				System.out.println(angle);
+		
+		p = new Point[N];
+		
+		for (int i = 0; i < N; i++)
+			p[i] = new Point(readInt(), readInt());
 
+		int ans = 0;
+		
+		for (int i = 0; i < N; i++) {
+			HashMap<Fraction, Integer> cnt = new HashMap<Fraction, Integer>();
+			
+			for (int j = 0; j < N; j++) {
+				if (i == j)
+					continue;
+				int dx = p[i].x - p[j].x;
+				int dy = p[i].y - p[j].y;
+				
+				Fraction f = new Fraction(dy, dx);
+				if (!cnt.containsKey(f))
+					cnt.put(f, 0);
+				cnt.put(f, cnt.get(f) + 1);
+				
+				if (cnt.get(new Fraction(-dx, dy)) != null)
+					ans += cnt.get(new Fraction(-dx, dy));
 			}
 		}
-		System.out.println(res);
+		
+		out.println(ans);
+		out.close();
 	}
 
-	private static Point curr;
-
-	private static Double angle (Point c, Point p) {
-		double angle = Math.toDegrees(Math.atan2(p.x - c.x, p.y - c.y));
-		if (angle < 0)
-			angle += 360;
-		return angle;
+	static int gcd (int a, int b) {
+		return b == 0 ? a : gcd(b, a % b);
 	}
-
-	static class Point implements Comparable<Point> {
+	
+	static class Fraction {
+		int num, den;
+		
+		Fraction (int num, int den) {
+			if (num == 0) {
+				den = 1;
+			} else if (den == 0) {
+				num = 0;
+			} else {
+				int gcd = gcd(num, den);
+				num /= gcd;
+				den /= gcd;
+				if (den < 0) {
+					den *= - 1;
+					num *= - 1;
+				}
+			}
+			
+			this.num = num;
+			this.den = den;
+		}
+		
+		@Override
+		public int hashCode () {
+			return num ^ den;
+		}
+		
+		@Override
+		public boolean equals (Object o) {
+			if (o instanceof Fraction) {
+				Fraction f = (Fraction)o;
+				return num == f.num && den == f.den;
+			}
+			return false;
+		}
+	}
+	
+	static class Point {
 		int x, y;
-
 		Point (int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
-
-		@Override
-		public int compareTo (Point o) {
-			return angle(curr, new Point(x, y)).compareTo(angle(curr, o));
-		}
-
-		public String toString () {
-			return "(" + x + "," + y + ")";
-		}
 	}
-
+	
 	static String next () throws IOException {
 		while (st == null || !st.hasMoreTokens())
 			st = new StringTokenizer(br.readLine().trim());
