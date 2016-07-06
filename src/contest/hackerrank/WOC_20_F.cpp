a8ac3c5c45a88df33e3a34ae2ab6caf0b6b28927
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-07-05 22:54:39 -0400

    Added other old C++ Solutions

---
+++ src/contest/hackerrank/WOC_20_F.cpp
@@ -0,0 +1,175 @@
+#include <bits/stdc++.h>
+
+#define mp make_pair
+#define pb push_back
+#define INF 1<<30
+#define MOD 1000000007
+#define rint(x) scanf("%d", &(x))
+#define rlong(x) scanf("%lld", &(x))
+#define SIZE 100100
+#define l(x) x << 1
+#define r(x) x << 1|1
+#define m(x, y) (x + y)/2
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+int N, D, K;
+int L[SIZE], R[SIZE], ans[SIZE];
+vector<vector<int>> occ (SIZE);
+vector<vector<int>> start (SIZE);
+
+struct Fenwick {
+    vector<int> values, tree;
+    int sz;
+
+    Fenwick () {
+        values.pb(-1);
+    }
+
+    void init (int x) {
+        for (int val : start[x])
+            values.pb(val);
+        sort(values.begin(), values.end());
+        values.resize(unique(values.begin(), values.end()) - values.begin());
+        tree.resize(values.size(), 1 << 30);
+        sz = values.size();
+    }
+
+    void init (vector<int> a, vector<int> b) {
+        int i = 1, j = 1, prev = -1;
+        while (i < a.size() || j < b.size()) {
+            int valToAdd = 0;
+            if (i == a.size() || (j != b.size() && a[i] > b[j])) {
+                valToAdd = b[j++];
+            } else {
+                valToAdd = a[i++];
+            }
+            if (valToAdd != prev)
+                values.pb(prev = valToAdd);
+        }
+
+        tree.resize(values.size(), 1 << 30);
+        sz = values.size();
+    }
+
+    int getIndex (int x) {
+        int lo = 0;
+        int hi = values.size() - 1;
+        while (lo <= hi) {
+            int mid = (lo + hi) >> 1;
+            if (values[mid] == x)
+                return mid;
+            else if (values[mid] < x)
+                lo = mid + 1;
+            else
+                hi = mid - 1;
+        }
+        return hi;
+    }
+
+    int query (int x) {
+        x = getIndex(x);
+
+        int ret = 1 << 30;
+
+        for (int i = x; i > 0; i -= (i & -i))
+            ret = min(ret, tree[i]);
+
+        return ret;
+    }
+
+    void update (int x, int val) {
+        x = getIndex(x);
+
+        if (x == 0)
+            return;
+
+        for (int i = x; i < sz; i += (i & -i))
+            tree[i] = min(tree[i], val);
+    }
+
+} seg[4 * SIZE];
+
+void init (int n, int l, int r) {
+    if (l == r) {
+        seg[n].init(l);
+        return;
+    }
+
+    int mid = (l + r) >> 1;
+
+    init(n << 1, l, mid);
+    init(n << 1 | 1, mid + 1, r);
+    seg[n].init(seg[n << 1].values, seg[n << 1 | 1].values);
+}
+
+void update (int n, int l, int r, int x, int val) {
+    seg[n].update(R[val], val);
+    if (l == x && r == x)
+        return;
+
+    int mid = (l + r) >> 1;
+
+    if (x <= mid)
+        update(n << 1, l, mid, x, val);
+    else
+        update(n << 1 | 1, mid + 1, r, x, val);
+}
+
+int query (int n, int l, int r, int ql, int qr, int val) {
+    if (l == ql && r == qr)
+        return seg[n].query(val);
+
+    int mid = (l + r) >> 1;
+
+    if (qr <= mid)
+        return query(n << 1, l, mid, ql, qr, val);
+    else if (ql > mid)
+        return query(n << 1 | 1, mid + 1, r, ql, qr, val);
+    return min(query(n << 1, l, mid, ql, mid, val), query(n << 1 | 1, mid + 1, r, mid + 1, qr, val));
+}
+
+int compute (int l, int r) {
+    if (l > r)
+        return 0;
+
+    int index = query(1, 1, D, l, r, r);
+
+    if (index == 1 << 30)
+        return 0;
+
+    return compute(l, L[index] - 1) + compute(R[index] + 1, r) + (R[index] - L[index] + 1);
+}
+
+int main () {
+	// freopen("in.txt", "r", stdin);
+	// freopen("out.txt", "w", stdout);
+
+    scanf("%d%d%d", &N, &D, &K);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d%d", &L[i], &R[i]);
+        start[L[i]].pb(R[i]);
+        occ[R[i] - L[i] + 1].pb(i);
+    }
+
+    init(1, 1, D);
+
+    for (int i = D; i >= 1; i--) {
+        for (int j : occ[i])
+            update(1, 1, D, L[j], j);
+        ans[i] = compute(1, D);
+    }
+
+    for (int i = 0; i < K; i++) {
+        int x;
+        scanf("%d", &x);
+        printf("%d\n", ans[x]);
+    }
+
+	return 0;
+}
---
+++ src/contest/misc/Benergy_Transfer.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+#define mp make_pair
+#define pb push_back
+#define INF 1<<30
+#define MOD 1000000007
+#define EPS 1e-7
+#define rint(x) scanf("%d", &(x))
+#define rlong(x) scanf("%lld", &(x))
+#define SIZE 100000
+#define l(x) x << 1
+#define r(x) x << 1|1
+#define m(x, y) (x + y)/2
+
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+int N;
+int a[SIZE], b[SIZE], va[SIZE], vb[SIZE];
+
+ld getDist (ld t) {
+    ld ret = 0;
+    for (int i = 0; i < N; i++) {
+        ld diff = (b[i] - a[i]) + t * (vb[i] - va[i]);
+        ret += diff * diff;
+    }
+    return ret;
+}
+
+ld ternarySearch (ld lo, ld hi) {
+    while (abs(hi - lo) >= EPS) {
+        ld mid1 = (lo * 2 + hi) / 3.0;
+        ld mid2 = (lo + hi * 2) / 3.0;
+
+        if (getDist(mid1) >= getDist(mid2))
+            lo = mid1;
+        else
+            hi = mid2;
+    }
+    return (lo + hi) / 2;
+
+}
+
+int main () {
+	// freopen("in.txt", "r", stdin);
+	// freopen("out.txt", "w", stdout);
+
+    rint(N);
+
+    for (int i = 0; i < N; i++)
+        rint(a[i]);
+    for (int i = 0; i < N; i++)
+        rint(b[i]);
+    for (int i = 0; i < N; i++)
+        rint(va[i]);
+    for (int i = 0; i < N; i++)
+        rint(vb[i]);
+
+    if (getDist(0.0) <= getDist(EPS))
+        printf("0\n");
+    else
+        printf("%lf", ternarySearch(0.0, 1e10));
+
+	return 0;
+}
