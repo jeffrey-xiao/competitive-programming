fd5a177a24b03cb6b6f80578b9b0950221905a61
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-09-26 20:00:48 -0400

    Fixed

--- src/codebook/math/Euclid.java
+++ src/codebook/math/Euclid.java
@@ -17,49 +17,111 @@ public class Euclid {
 	}
 
 	// returns d = gcd(a, b); finds x, y such that d = ax * by
-	static int x, y;
-	static int extendedEuclid (int a, int b) {
-		int xx = y = 0;
-		int yy = x = 1;
-		while (b > 0) {
+	public static int[] euclid (int a, int b) {
+		int x = 1, y = 0, x1 = 0, y1 = 1, t;
+		while (b != 0) {
 			int q = a / b;
-			int t = b; b = a % b; a = t;
-			t = xx; xx = x - q * xx; a = t;
-			t = yy; yy = y - q * yy; y = t;
+			t = x;
+			x = x1;
+			x1 = t - q * x1;
+			t = y;
+			y = y1;
+			y1 = t - q * y1;
+			t = b;
+			b = a - q * b;
+			a = t;
 		}
-		return a;
+		return a > 0 ? new int[] {a, x, y} : new int[] {-a, -x, -y};
+	}
+
+	public static int[] euclid2 (int a, int b) {
+		if (b == 0)
+			return a > 0 ? new int[] {a, 1, 0} : new int[] {-a, -1, 0};
+		int[] r = euclid2(b, a % b);
+		return new int[] {r[0], r[2], r[1] - a / b * r[2]};
 	}
 
 	// finds all solutions to ax = b mod n
 	static ArrayList<Integer> linearEquationSolver (int a, int b, int n) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
+		int[] res = euclid(a, b);
+		int d = res[0], x = res[1];
 
-		int d = extendedEuclid(a, b);
 		if (b % d == 0) {
 			x = mod(x * (b / d), n);
 			for (int i = 0; i < d; i++)
 				ret.add(mod(x + i * (n / d), n));
 		}
+
 		return ret;
 	}
 
-	// computes b such that ab = 1 (mod n), returns -1 on failure
-	static int modInverse (int a, int n) {
-		int d = extendedEuclid(a, n);
-		if (d > 1)
-			return -1;
-		return mod(x, n);
+	// computes x and y such that ax + by = c; on failure, x = y = -1 << 30
+	static int[] linearDiophantine (int a, int b, int c) {
+		int d = gcd(a, b);
+		int x, y;
+
+		if (c % d != 0) {
+			x = y = -1 << 30;
+		} else {
+			a /= d;
+			b /= d;
+			c /= d;
+			int[] ret = euclid(a, b);
+			x = ret[1] * c;
+			y = ret[2] * c;
+		}
+
+		return new int[] {x, y};
 	}
 
-	// computes x and y such that ax + by = c; on failure, x = y = -1
-	static void linearDiophantine(int a, int b, int c) {
-		int d = gcd(a, b);
-		if (c % d != 0) {
-			x = y = -1;
-		} else {
-			x = c / d * modInverse(a / d, b / d);
-			y = (c - a * x) / b;
+	// precondition: m > 0 && gcd(a, m) = 1
+	public static int modInverse (int a, int m) {
+		a = mod(a, m);
+		return a == 0 ? 0 : mod((1 - modInverse(m % a, a) * m) / a, m);
+	}
+
+	// precondition: m > 0 && gcd(a, m) = 1
+	public static int modInverse2 (int a, int m) {
+		return (euclid(a, m)[1] % m + m) % m;
+	}
+
+	// precondition: p is prime
+	public static int[] generateInverse (int p) {
+		int[] res = new int[p];
+		res[1] = 1;
+		for (int i = 2; i < p; ++i)
+			res[i] = (p - (p / i) * res[p % i] % p) % p;
+		return res;
+	}
+
+	// solve x = a[i] (mod p[i]), where gcd(p[i], p[j]) == 1
+	public static int simpleRestore (int[] a, int[] p) {
+		int res = a[0];
+		int m = 1;
+		for (int i = 1; i < a.length; i++) {
+			m *= p[i - 1];
+			while (res % p[i] != a[i])
+				res += m;
 		}
+		return res;
+	}
+
+	public static int garnerRestore (int[] a, int[] p) {
+		int[] x = new int[a.length];
+		for (int i = 0; i < x.length; ++i) {
+			x[i] = a[i];
+			for (int j = 0; j < i; ++j) {
+				x[i] = (int) modInverse(p[j], p[i]) * (x[i] - x[j]);
+				x[i] = (x[i] % p[i] + p[i]) % p[i];
+			}
+		}
+		int res = x[0];
+		int m = 1;
+		for (int i = 1; i < a.length; i++) {
+			m *= p[i - 1];
+			res += x[i] * m;
+		}
+		return res;
 	}
 }
-
