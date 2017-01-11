dac5b15a7b15bb26d8b565e4cba12ac200610e8a
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-01-11 01:19:05 -0500

    Added Facebook Hacker Cup Qualification

---
+++ src/contest/hackercup/Lazy_Loading.java
@@ -0,0 +1,75 @@
+package contest.hackercup;
+
+import java.util.*;
+import java.io.*;
+
+public class Lazy_Loading {
+
+	static BufferedReader br;
+	static PrintWriter out;
+	static StringTokenizer st;
+
+	static int T, N;
+	static int[] W;
+	
+	public static void main (String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		br = new BufferedReader(new FileReader("in.txt"));
+		out = new PrintWriter(new FileWriter("out.txt"));
+
+		T = readInt();
+		
+		for (int t = 1; t <= T; t++) {
+			N = readInt();
+			W = new int[N];
+			
+			for (int i = 0; i < N; i++)
+				W[i] = readInt();
+			
+			Arrays.sort(W);
+			
+			int l = 0;
+			int r = N - 1;
+			int ans = 0;
+			while (r >= l) {
+				int itemsNeeded = 49 / W[r];
+				if (l + itemsNeeded > r)
+					break;
+				l += itemsNeeded;
+				r--;
+				ans++;
+			}
+			out.printf("Case #%d: %d\n", t, ans);
+		}
+		
+		out.close();
+	}
+
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
+
---
+++ src/contest/hackercup/Progress_Pie.java
@@ -0,0 +1,70 @@
+package contest.hackercup;
+
+import java.util.*;
+import java.io.*;
+
+public class Progress_Pie {
+
+	static BufferedReader br;
+	static PrintWriter out;
+	static StringTokenizer st;
+
+	static int T;
+	static double P, x, y;
+	
+	public static void main (String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(new OutputStreamWriter(System.out));
+		br = new BufferedReader(new FileReader("in.txt"));
+		out = new PrintWriter(new FileWriter("out.txt"));
+
+		T = readInt();
+		
+		for (int t = 1; t <= T; t++) {
+			P = readDouble() / 100.0 * Math.PI * 2;
+			x = readInt() - 50;
+			y = readInt() - 50;
+			
+			double angle = Math.PI / 2 - Math.atan2(y, x);
+			if (angle < 0)
+				angle += 2 * Math.PI;
+			if (P == 0)
+				out.printf("Case #%d: white\n", t);
+			else if (x == 0 && y == 0)
+				out.printf("Case #%d: black\n", t);
+			else if (x * x + y * y > 50 * 50 || angle > P)
+				out.printf("Case #%d: white\n", t);
+			else
+				out.printf("Case #%d: black\n", t);
+		}
+		
+		out.close();
+	}
+
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
+
