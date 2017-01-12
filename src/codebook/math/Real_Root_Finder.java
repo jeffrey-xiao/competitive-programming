6cc5667b47f72eaa3d03ae87caafbffe5c6126ab
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-01-12 00:18:26 -0500

    Added root finding

--- src/codebook/math/Real_Root_Finder.java
+++ src/codebook/math/Real_Root_Finder.java
@@ -1,97 +1,44 @@
 package codebook.math;
 
-import java.util.*;
-
 public class Real_Root_Finder {
 
 	static final double EPS = 1e-9;
-	static final double INF = 1e30;
-
-	static class Monomial {
-		double coefficient, exponent;
-
-		Monomial (double coefficient, double exponent) {
-			this.coefficient = coefficient;
-			this.exponent = exponent;
-		}
-
-		double evaluate (double x) {
-			return coefficient * Math.pow(x, exponent);
-		}
-
-		public String toString () {
-			return String.format("%fx^%f", coefficient, exponent);
-		}
+	
+	interface Function {
+		public abstract double eval (double x); 
 	}
-
-	static double evaluate (ArrayList<Monomial> polynomial, double x) {
-		double ret = 0;
-		for (Monomial m : polynomial)
-			ret += m.evaluate(x);
-		return ret;
+	
+	// Finds a root of function f with derivative d based on initial guess x0 
+	public static double newton (double x0, Function f, Function d) {
+		double prev = x0, next = x0;
+		do {
+			prev = next;
+			next = prev - f.eval(prev) / d.eval(prev);
+		} while (Math.abs(next - prev) > EPS);
+		return next;
 	}
-
-	static double findRoot (ArrayList<Monomial> polynomial, double x1, double x2) {
-		double y1 = evaluate(polynomial, x1), y2 = evaluate(polynomial, x2);
-		boolean neg1 = y1 < 0, neg2 = y2 < 0;
-
-		if (Math.abs(y1) <= EPS)
-			return x1;
-		if (Math.abs(y2) <= EPS)
-			return x2;
-
-		if (neg1 == neg2)
-			return Double.NaN;
-
-		while (x2 - x1 > EPS) {
-			double x = (x2 + x1) / 2;
-			if ((evaluate(polynomial, x) < 0) == neg1)
-				x1 = x;
-			else
-				x2 = x;
-		}
-		return x1;
+	
+	// Finds a root of function f(x) = 0 by rearranging it into
+	// g(x) = x and repeatedly plugging x into g(x)
+	// Does not work if |f'(x0)| >= 1
+	public static double fixedPointIteration (double x0, Function f) {
+		double prev = x0, next = x0;
+		do {
+			prev = next;
+			next = f.eval(prev);
+		} while (Math.abs(next - prev) > EPS);
+		return next;
 	}
-
-	static ArrayList<Double> findAllRoots (ArrayList<Monomial> polynomial) {
-		ArrayList<Monomial> diff = new ArrayList<Monomial>();
-		ArrayList<Double> ret = new ArrayList<Double>();
-
-		for (Monomial m : polynomial)
-			if (m.exponent > 0)
-				diff.add(new Monomial(m.coefficient * m.exponent, m.exponent - 1));
-
-		if (diff.isEmpty())
-			return ret;
-
-		ArrayList<Double> diffRoots = findAllRoots(diff);
-		diffRoots.add(0, -INF);
-		diffRoots.add(INF);
-
-		for (int i = 0; i < diffRoots.size() - 1; i++) {
-			Double root = findRoot(polynomial, diffRoots.get(i), diffRoots.get(i + 1));
-			if (Double.isNaN(root))
-				continue;
-			if (ret.isEmpty() || Math.abs(ret.get(ret.size() - 1) - root) > EPS)
-				ret.add(root);
-		}
-		return ret;
-	}
-
+	
 	public static void main (String[] args) {
-		ArrayList<Monomial> polynomial = new ArrayList<Monomial>();
-		polynomial.add(new Monomial(2, 3));
-		polynomial.add(new Monomial(-54, 0));
-
-		// [2.999999999962339]
-		System.out.println(findAllRoots(polynomial).toString());
-
-		polynomial = new ArrayList<Monomial>();
-		polynomial.add(new Monomial(3, 2));
-		polynomial.add(new Monomial(4, 1));
-		polynomial.add(new Monomial(-20, 0));
-
-		// [-3.3333333336180133, 1.9999999997299984]
-		System.out.println(findAllRoots(polynomial).toString());
+		Function f = (double x) -> (x * x * x * x - 15 * x + 2);
+		Function d = (double x) -> (4 * x * x * x - 15);
+		Function g = (double x) -> ((x * x * x * x + 2) / 15);
+		
+		// 0.13335441662206313
+		System.out.println(newton(1, f, d));
+		// 0.13335441662208475
+		System.out.println(fixedPointIteration(1, g));
 	}
 }
+
