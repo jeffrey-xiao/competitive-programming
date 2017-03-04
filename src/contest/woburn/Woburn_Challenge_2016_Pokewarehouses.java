f2f741d66f2094c083be115d48f742ea6b8e2922
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-03-03 19:56:54 -0500

    Updated woburn challenge

--- src/contest/woburn/Woburn_Challenge_2016_Pokewarehouses.java
+++ src/contest/woburn/Woburn_Challenge_2016_Pokewarehouses.java
@@ -9,12 +9,41 @@ public class Woburn_Challenge_2016_Pokewarehouses {
 	static PrintWriter out;
 	static StringTokenizer st;
 
+	static int N, ans;
+	static Stack<Integer> s = new Stack<Integer>();
+	static int[] val;
+	
 	public static void main (String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		out = new PrintWriter(new OutputStreamWriter(System.out));
 		//br = new BufferedReader(new FileReader("in.txt"));
 		//out = new PrintWriter(new FileWriter("out.txt"));
 
+		N = readInt();
+		val = new int[N];
+		
+		for (int i = 0; i < N; i++)
+			val[i] = readInt();
+		
+		int currValue = N;
+		int currIndex = 0;
+		while (currValue > 0) {
+			if (currIndex < N && val[currIndex] == currValue) {
+				currValue--;
+				currIndex++;
+			} else if (!s.isEmpty() && s.peek() == currValue) {
+				currValue--;
+				s.pop();
+			} else if (currIndex < N) {
+				ans = 1;
+				s.push(val[currIndex++]);
+			} else {
+				ans = 2;
+				break;
+			}
+		}
+		
+		out.println(ans);
 		out.close();
 	}
 
