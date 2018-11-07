d0dfc8f3f3a74219ecf16520d6021de04a2bc9f6
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2018-11-06 23:15:26 -0500

    Add a few quora solutions

---
+++ src/contest/quora/RelatedQuestions.java
@@ -0,0 +1,123 @@
+package quora;
+
+import java.io.*;
+import java.util.*;
+
+public class RelatedQuestions {
+
+  static BufferedReader br;
+  static PrintWriter out;
+  static StringTokenizer st;
+
+  static double[] subtreeReadTime, totalReadTime, questionReadTime;
+  static ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+  static int N;
+
+  public static void main (String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    out = new PrintWriter(new OutputStreamWriter(System.out));
+    // br = new BufferedReader(new FileReader("in.txt"));
+    // out = new PrintWriter(new FileWriter("out.txt"));
+
+    N = readInt();
+
+    questionReadTime = new double[N];
+    subtreeReadTime = new double[N];
+    totalReadTime = new double[N];
+
+    for (int i = 0; i < N; i++) {
+      questionReadTime[i] = readInt();
+      adj.add(new ArrayList<Integer>());
+    }
+
+    for (int i = 0; i < N - 1; i++) {
+      int u = readInt() - 1;
+      int v = readInt() - 1;
+      adj.get(u).add(v);
+      adj.get(v).add(u);
+    }
+
+    computeSubtree(2, -1);
+    computeTotal(2, -1);
+    
+    int minIndex = 0;
+    for (int i = 1; i < N; i++) {
+      if (totalReadTime[i] < totalReadTime[minIndex]) {
+        minIndex = i;
+      }
+    }
+
+    out.println(minIndex + 1);
+    out.close();
+  }
+
+  // compute subtree read times
+  static void computeSubtree(int u, int par) {
+    subtreeReadTime[u] = questionReadTime[u];
+    for (int v : adj.get(u)) {
+      if (v == par) {
+        continue;
+      }
+      computeSubtree(v, u);
+      subtreeReadTime[u] += subtreeReadTime[v] / (adj.get(u).size() - (par == -1 ? 0 : 1));
+    }
+  }
+
+  // compute total read times
+  static void computeTotal(int u, int par) {
+    if (par == -1) {
+      totalReadTime[u] = subtreeReadTime[u];
+    } else {
+      int parSize = adj.get(par).size();
+      int size = adj.get(u).size();
+      if (size == 1 && parSize == 1) {
+        totalReadTime[u] = questionReadTime[u] + questionReadTime[par];
+      } else if (size == 1) {
+        totalReadTime[u] = questionReadTime[u];
+        totalReadTime[u] += ((totalReadTime[par] - questionReadTime[par]) - questionReadTime[u] / parSize) * parSize / (parSize - 1) + questionReadTime[par];
+      } else if (parSize == 1) {
+        totalReadTime[u] = questionReadTime[u];
+        totalReadTime[u] += (subtreeReadTime[u] - questionReadTime[u]) * (size - 1) / size;
+        totalReadTime[u] += questionReadTime[par] / size;
+      } else {
+        totalReadTime[u] = questionReadTime[u];
+        totalReadTime[u] += (subtreeReadTime[u] - questionReadTime[u]) * (size - 1) / size;
+        totalReadTime[u] += (((totalReadTime[par] - questionReadTime[par]) - subtreeReadTime[u] / parSize) * parSize / (parSize - 1) + questionReadTime[par]) / size;
+      }
+    }
+
+    for (int v : adj.get(u)) {
+      if (v == par) {
+        continue;
+      }
+      computeTotal(v, u);
+    }
+  }
+  
+
+  static String next () throws IOException {
+    while (st == null || !st.hasMoreTokens())
+      st = new StringTokenizer(br.readLine().trim());
+    return st.nextToken();
+  }
+
+  static long readLong () throws IOException {
+    return Long.parseLong(next());
+  }
+
+  static int readInt () throws IOException {
+    return Integer.parseInt(next());
+  }
+
+  static double readDouble () throws IOException {
+    return Double.parseDouble(next());
+  }
+
+  static char readCharacter () throws IOException {
+    return next().charAt(0);
+  }
+
+  static String readLine () throws IOException {
+    return br.readLine().trim();
+  }
+}
---
+++ src/contest/quora/Upvotes.java
@@ -0,0 +1,156 @@
+package quora;
+
+import java.io.*;
+import java.util.*;
+
+public class Upvotes {
+
+  static BufferedReader br;
+  static PrintWriter out;
+  static StringTokenizer st;
+
+  public static void main (String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    out = new PrintWriter(new OutputStreamWriter(System.out));
+    // br = new BufferedReader(new FileReader("in.txt"));
+    // out = new PrintWriter(new FileWriter("out.txt"));
+
+    int N = readInt();
+    int K = readInt();
+
+    int[] upvotes = new int[N];
+    ArrayDeque<Segment> inc = new ArrayDeque<Segment>(), dec = new ArrayDeque<Segment>();
+
+    for (int i = 0; i < N; i++) {
+      upvotes[i] = readInt();
+    }
+
+    // Adding increasing segments
+    int i = 0;
+    while (i < N) {
+      int l = i;
+      int r = i;
+      while (r + 1 < N && upvotes[r + 1] >= upvotes[r]) {
+        r++;
+      }
+      inc.add(new Segment(l, r));
+      i = r + 1;
+    }
+
+    // Adding decreasing segments
+    i = 0;
+    while (i < N) {
+      int l = i;
+      int r = i;
+      while (r + 1 < N && upvotes[r + 1] <= upvotes[r]) {
+        r++;
+      }
+      dec.add(new Segment(l, r));
+      i = r + 1;
+    }
+
+    ArrayDeque<Segment> currInc = new ArrayDeque<Segment>(), currDec = new ArrayDeque<Segment>();
+    long currTotal = 0;
+    for (i = 0; i < N - K + 1; i++) {
+      long l = i;
+      long r = i + K - 1;
+      // popping in
+      while (!inc.isEmpty() && inc.peekFirst().l <= r) {
+        Segment s = inc.pollFirst();
+        currTotal += s.segments();
+        currInc.addLast(s);
+      }
+      while (!dec.isEmpty() && dec.peekFirst().l <= r) {
+        Segment s = dec.pollFirst();
+        currTotal -= s.segments();
+        currDec.addLast(s);
+      }
+
+      // popping out
+      while (!currInc.isEmpty() && currInc.peekFirst().r < l) {
+        Segment s = currInc.pollFirst();
+        currTotal -= s.segments();
+      }
+      while (!currDec.isEmpty() && currDec.peekFirst().r < l) {
+        Segment s = currDec.pollFirst();
+        currTotal += s.segments();
+      }
+
+      // Manually calculate ends
+      long ans = currTotal;
+
+      if (currInc.size() == 1) {
+        Segment s = currInc.peekFirst();
+        ans -= s.segments() - (r - l + 1) * (r - l) / 2;
+      } else {
+        Segment s = currInc.peekFirst();
+        ans -= s.segments() - (s.r - l + 1) * (s.r - l) / 2;
+        s = currInc.peekLast();
+        ans -= s.segments() - (r - s.l + 1) * (r - s.l) / 2;
+      }
+
+      if (currDec.size() == 1) {
+        Segment s = currDec.peekFirst();
+        ans += s.segments() - (r - l + 1) * (r - l) / 2;
+      } else {
+        Segment s = currDec.peekFirst();
+        ans += s.segments() - (s.r - l + 1) * (s.r - l) / 2;
+        s = currDec.peekLast();
+        ans += s.segments() - (r - s.l + 1) * (r - s.l) / 2;
+      }
+
+
+      out.println(ans);
+    }
+
+    out.close();
+  }
+
+  static class Segment {
+    int l, r;
+
+    Segment(int l, int r) {
+      this.l = l;
+      this.r = r;
+    }
+
+    public long dist() {
+      return r - l + 1;
+    }
+
+    public long segments() {
+      return dist() * (dist() - 1) / 2;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("[%d, %d]", l, r);
+    }
+  }
+
+  static String next () throws IOException {
+    while (st == null || !st.hasMoreTokens())
+      st = new StringTokenizer(br.readLine().trim());
+    return st.nextToken();
+  }
+
+  static long readLong () throws IOException {
+    return Long.parseLong(next());
+  }
+
+  static int readInt () throws IOException {
+    return Integer.parseInt(next());
+  }
+
+  static double readDouble () throws IOException {
+    return Double.parseDouble(next());
+  }
+
+  static char readCharacter () throws IOException {
+    return next().charAt(0);
+  }
+
+  static String readLine () throws IOException {
+    return br.readLine().trim();
+  }
+}
