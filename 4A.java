d8ca9efe9762d9953bcacbc1ca1fe0da5cbe6ac4
ANUJ YADAV <52133014+ANUJ581@users.noreply.github.com>
2019-10-06 10:24:47 +0530

    Add files via upload

---
+++ 4A.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+ 
+public class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if(n> 2 && n % 2 == 0)
+			System.out.println("YES");
+		else
+			System.out.println("NO");
+ 
+		}
+}
